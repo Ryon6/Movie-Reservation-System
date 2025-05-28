a0c947b79e34e885b9c5c0753e7dd345c1f72d96
peng <peng.nullptr@qq.com>
2025-05-29 00:57:24 +0800

    feat(db): 添加 DBConnectionFactory 接口以支持数据库连接创建

---
+++ internal/infrastructure/persistence/db_factory.go
@@ -0,0 +1,13 @@
+package persistence
+
+import (
+	"mrs/internal/infrastructure/config"
+
+	"gorm.io/gorm"
+)
+
+// 工厂模式
+// DBConnectionFactory 是一个接口，用于创建数据库连接。
+type DBConnectionFactory interface {
+	CreateDBConnection(dbConfig config.DatabaseConfig) (*gorm.DB, error)
+}
