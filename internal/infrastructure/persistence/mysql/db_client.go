6174d4599da988c3d43cac5867639d83740c426b
peng <peng.nullptr@qq.com>
2025-05-29 00:57:33 +0800

    feat(mysql): 添加 mysqlDBFactory 以支持 MySQL 数据库连接创建

---
+++ internal/infrastructure/persistence/mysql/db_client.go
@@ -0,0 +1,85 @@
+// TODO: 更具扩展性的数据库配置
+package mysql
+
+import (
+	"fmt"
+	"log"
+	"mrs/internal/infrastructure/config"
+	applog "mrs/pkg/log"
+	"os"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type mysqlDBFactary struct {
+	logger applog.Logger
+}
+
+func NewMysqlDBFactory(logger applog.Logger) *mysqlDBFactary {
+	return &mysqlDBFactary{
+		logger: logger,
+	}
+}
+
+func (f *mysqlDBFactary) CreateDBConnection(dbConfig config.DatabaseConfig) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=True&loc=Local",
+		dbConfig.User,
+		dbConfig.Password,
+		dbConfig.Host,
+		dbConfig.Port,
+		dbConfig.Name,
+		dbConfig.Charset,
+	)
+
+	var gormLogLevel logger.LogLevel
+	if dbConfig.LogMode {
+		gormLogLevel = logger.Info
+	} else {
+		gormLogLevel = logger.Silent
+	}
+
+	gormLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold:             200 * time.Millisecond,
+			LogLevel:                  gormLogLevel,
+			IgnoreRecordNotFoundError: true,
+			Colorful:                  false,
+		},
+	)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: gormLogger,
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf(("failed to connect to MySQL: %w"), err)
+	}
+
+	// 设置连接池参数
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get database instance: %w", err)
+	}
+
+	if dbConfig.MaxIdleConnections > 0 {
+		sqlDB.SetMaxIdleConns(dbConfig.MaxIdleConnections)
+	}
+	if dbConfig.MaxOpenConnections > 0 {
+		sqlDB.SetMaxOpenConns(dbConfig.MaxOpenConnections)
+	}
+	if dbConfig.ConnMaxLifetimeMinutes > 0 {
+		sqlDB.SetConnMaxLifetime(time.Duration(dbConfig.ConnMaxLifetimeMinutes) * time.Minute)
+	}
+
+	if err := sqlDB.Ping(); err != nil {
+		sqlDB.Close() // 确保在失败时关闭连接
+		return nil, fmt.Errorf("failed to ping MySQL: %w", err)
+	}
+
+	f.logger.Info("MySQL database connection established successfully", applog.String("dsn", dsn))
+	return db, nil
+}
