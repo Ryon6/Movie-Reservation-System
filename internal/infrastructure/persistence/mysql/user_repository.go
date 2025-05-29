e37d9f4b086fa4107e9035a90b5e149e4d6e9c90
peng <peng.nullptr@qq.com>
2025-05-29 18:23:25 +0800

    feat(user): 实现用户存储库，添加创建、查找、更新和删除用户功能

---
+++ internal/infrastructure/persistence/mysql/user_repository.go
@@ -0,0 +1,102 @@
+package mysql
+
+import (
+	"context"
+	"errors"
+	"mrs/internal/domain/user"
+	applog "mrs/pkg/log"
+
+	"gorm.io/gorm"
+)
+
+type gormUserRepository struct {
+	db     *gorm.DB
+	logger applog.Logger
+}
+
+func NewGormUserRepository(db *gorm.DB, logger applog.Logger) *gormUserRepository {
+	return &gormUserRepository{
+		db:     db,
+		logger: logger,
+	}
+}
+
+// 创建用户
+func (r *gormUserRepository) Create(ctx context.Context, user *user.User) error {
+	logger := r.logger.With(applog.String("Method", "gormUserRepository.Create"))
+	if err := r.db.WithContext(ctx).Create(user).Error; err != nil {
+		logger.Error("failed to create user", applog.Error(err))
+	}
+	logger.Info("user created successfully", applog.Uint("user_id", user.ID))
+	return nil
+}
+
+// 通过ID获取用户，使用场景需要完整的用户信息，包括关联的角色信息。
+func (r *gormUserRepository) FindByID(ctx context.Context, id uint) (*user.User, error) {
+	logger := r.logger.With(applog.String("Method", "gormUserRepository.FindByID"))
+	var user user.User
+	// Preload("Role") 会根据 User 结构体中的 Role 字段和外键 RoleID 加载关联的角色信息。
+	if err := r.db.WithContext(ctx).Preload("Role").First(&user, id).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			logger.Warn("user not found by ID", applog.Uint("user_id", id))
+			return nil, err
+		}
+		logger.Error("failed to find user by ID", applog.Uint("user_id", id), applog.Error(err))
+		return nil, err
+	}
+	logger.Info("user found by ID", applog.Uint("ID", user.ID))
+	return &user, nil
+}
+
+// 通过用户名获取用户，使用场景需要完整的用户信息，包括关联的角色信息。
+func (r *gormUserRepository) FindByUsername(ctx context.Context, username string) (*user.User, error) {
+	logger := r.logger.With(applog.String("Method", "gormUserRepository.FindByUsername"))
+	var user user.User
+	if err := r.db.WithContext(ctx).Preload("Role").Where("username = ?", username).First(&user).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			logger.Warn("user not found by username", applog.String("username", username))
+			return nil, err
+		}
+		logger.Error("failed to find user by username", applog.String("username", username), applog.Error(err))
+		return nil, err
+	}
+	logger.Info("user found by username", applog.String("username", user.Username))
+	return &user, nil
+}
+
+// 通过邮箱获取用户，使用场景需要完整的用户信息，包括关联的角色信息。
+func (r *gormUserRepository) FindByEmail(ctx context.Context, email string) (*user.User, error) {
+	logger := r.logger.With(applog.String("Method", "gormUserRepository.FindByEmail"))
+	var user user.User
+	if err := r.db.WithContext(ctx).Preload("Role").Where("email = ?", email).First(&user).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			logger.Warn("user not found by email", applog.String("email", email))
+			return nil, err
+		}
+		logger.Error("failed to find user by email", applog.String("email", email), applog.Error(err))
+		return nil, err
+	}
+	logger.Info("user found by email", applog.String("email", user.Email))
+	return &user, nil
+}
+
+// Update 更新用户信息
+func (r *gormUserRepository) Update(ctx context.Context, user *user.User) error {
+	logger := r.logger.With(applog.String("Method", "gormUserRepository.Update"))
+	if err := r.db.WithContext(ctx).Save(user).Error; err != nil {
+		logger.Error("failed to update user", applog.Uint("user_id", user.ID), applog.Error(err))
+		return err
+	}
+	logger.Info("user updated successfully", applog.Uint("user_id", user.ID))
+	return nil
+}
+
+// Delete 删除用户
+func (r *gormUserRepository) Delete(ctx context.Context, id uint) error {
+	logger := r.logger.With(applog.String("Method", "gormUserRepository.Delete"))
+	if err := r.db.WithContext(ctx).Delete(&user.User{}, id).Error; err != nil {
+		logger.Error("failed to delete user", applog.Uint("user_id", id), applog.Error(err))
+	}
+	logger.Info("user delete successfully", applog.Uint("user_id", id))
+	return nil
+}
