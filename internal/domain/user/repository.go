56a3aec5cf0b64e9541a0f684e8c5d437d8f5d1f
peng <peng.nullptr@qq.com>
2025-05-29 15:11:44 +0800

    feat(user): 更新用户结构体，添加全名和最后登录时间字段，重构密码处理方法

--- internal/domain/user/repository.go
+++ internal/domain/user/repository.go
@@ -1,92 +1,92 @@
 package user
 
 import (
-	"go.uber.org/zap"
-	"gorm.io/gorm"
+	"context"
 )
 
+// UserRepository 定义了用户数据持久化操作的接口。
 type UserRepository interface {
-	CreateUser(user *User) error
-	GetUserById(id uint)
-	GetUserByUsername(username string)
-	UpdateUser(user *User)
-	DeleteUser(id uint)
-	GetUsersByRole(roleName string)
+	Create(ctx context.Context, user *User) error                       // 创建用户
+	FindByID(ctx context.Context, id uint) (*User, error)               // 通过ID获取用户
+	FindByUsername(ctx context.Context, username string) (*User, error) // 通过name获取用户
+	FindByEmail(ctx context.Context, email string) (*User, error)       // 通过name获取用户
+	Update(ctx context.Context, user *User) error                       // 更新用户
+	Delete(ctx context.Context, id uint) error                          // 删除用户
 }
 
-type userRepository struct {
-	db     *gorm.DB
-	logger *zap.Logger
-}
+// type userRepository struct {
+// 	db     *gorm.DB
+// 	logger *zap.Logger
+// }
 
-func NewUserRepository(db *gorm.DB, logger *zap.Logger) *userRepository {
-	return &userRepository{
-		db:     db,
-		logger: logger,
-	}
-}
+// func NewUserRepository(db *gorm.DB, logger *zap.Logger) *userRepository {
+// 	return &userRepository{
+// 		db:     db,
+// 		logger: logger,
+// 	}
+// }
 
-// 创建用户
-func (r *userRepository) CreateUser(user *User) error {
-	Result := r.db.Create(&user)
-	if Result.Error != nil {
-		r.logger.Error("Failed to create user", zap.Error(Result.Error))
-		return Result.Error
-	}
-	return nil
-}
+// // 创建用户
+// func (r *userRepository) CreateUser(user *User) error {
+// 	Result := r.db.Create(&user)
+// 	if Result.Error != nil {
+// 		r.logger.Error("Failed to create user", zap.Error(Result.Error))
+// 		return Result.Error
+// 	}
+// 	return nil
+// }
 
-// 通过 ID 获取用户
-func (r *userRepository) GetUserById(id uint) (*User, error) {
-	var user User
-	Result := r.db.First(&user, id)
-	if Result.Error != nil {
-		r.logger.Error("Failed to get user by Id", zap.Uint("UserId", id), zap.Error(Result.Error))
-		return nil, Result.Error
-	}
-	return &user, nil
-}
+// // 通过 ID 获取用户
+// func (r *userRepository) GetUserById(id uint) (*User, error) {
+// 	var user User
+// 	Result := r.db.First(&user, id)
+// 	if Result.Error != nil {
+// 		r.logger.Error("Failed to get user by Id", zap.Uint("UserId", id), zap.Error(Result.Error))
+// 		return nil, Result.Error
+// 	}
+// 	return &user, nil
+// }
 
-// 通过用户名获取用户
-func (r *userRepository) GetUserByUsername(username string) (*User, error) {
-	var user User
-	Result := r.db.Where("username = ?", username).First(&user)
-	if Result.Error != nil {
-		r.logger.Error("Failed to get user by username", zap.String("username", username), zap.Error(Result.Error))
-		return nil, Result.Error
-	}
-	return &user, nil
-}
+// // 通过用户名获取用户
+// func (r *userRepository) GetUserByUsername(username string) (*User, error) {
+// 	var user User
+// 	Result := r.db.Where("username = ?", username).First(&user)
+// 	if Result.Error != nil {
+// 		r.logger.Error("Failed to get user by username", zap.String("username", username), zap.Error(Result.Error))
+// 		return nil, Result.Error
+// 	}
+// 	return &user, nil
+// }
 
-// 更新用户
-func (r *userRepository) UpdateUser(user *User) error {
-	Result := r.db.Save(&user)
-	if Result.Error != nil {
-		r.logger.Error("Failed to update user", zap.Uint("UserId", user.ID), zap.Error(Result.Error))
-		return Result.Error
-	}
-	return nil
-}
+// // 更新用户
+// func (r *userRepository) UpdateUser(user *User) error {
+// 	Result := r.db.Save(&user)
+// 	if Result.Error != nil {
+// 		r.logger.Error("Failed to update user", zap.Uint("UserId", user.ID), zap.Error(Result.Error))
+// 		return Result.Error
+// 	}
+// 	return nil
+// }
 
-// 删除用户
-func (r *userRepository) DeleteUser(id uint) error {
-	Result := r.db.Delete(id)
-	if Result.Error != nil {
-		r.logger.Error("Failed to delete user", zap.Uint("UserId", id), zap.Error(Result.Error))
-		return Result.Error
-	}
-	return nil
-}
+// // 删除用户
+// func (r *userRepository) DeleteUser(id uint) error {
+// 	Result := r.db.Delete(id)
+// 	if Result.Error != nil {
+// 		r.logger.Error("Failed to delete user", zap.Uint("UserId", id), zap.Error(Result.Error))
+// 		return Result.Error
+// 	}
+// 	return nil
+// }
 
-// 获取具有特定角色的用户
-func (r *userRepository) GetUsersByRole(roleName string) ([]*User, error) {
-	var users []*User
-	Result := r.db.Joins("JOIN roles ON roles.id = users.role_id").
-		Where("roles.name = ?", roleName).
-		Find(&users)
-	if Result.Error != nil {
-		r.logger.Error("Failed to get users by role", zap.String("roleName", roleName), zap.Error(Result.Error))
-		return nil, Result.Error
-	}
-	return users, nil
-}
+// // 获取具有特定角色的用户
+// func (r *userRepository) GetUsersByRole(roleName string) ([]*User, error) {
+// 	var users []*User
+// 	Result := r.db.Joins("JOIN roles ON roles.id = users.role_id").
+// 		Where("roles.name = ?", roleName).
+// 		Find(&users)
+// 	if Result.Error != nil {
+// 		r.logger.Error("Failed to get users by role", zap.String("roleName", roleName), zap.Error(Result.Error))
+// 		return nil, Result.Error
+// 	}
+// 	return users, nil
+// }
--- internal/domain/user/user.go
+++ internal/domain/user/user.go
@@ -1,16 +1,38 @@
 package user
 
 import (
+	"fmt"
 	"mrs/internal/domain/role"
+	"time"
 
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
 type User struct {
 	gorm.Model
-	Username     string `gorm:"uniqueIndex"`
-	PasswordHash string
-	Email        string `gorm:"uniqueIndex"`
-	RoleId       uint
-	Role         role.Role
+	Username     string     `gorm:"varchar(100),uniqueIndex,not null"`
+	PasswordHash string     `gorm:"varchar(255),not null"`             // 存储密码的哈希值
+	Email        string     `gorm:"varchar(255),uniqueIndex,not null"` // 用户邮箱，唯一索引
+	FullName     string     `gorm:"varchar(100)"`                      // 用户全名（可选）
+	LastLogin    *time.Time // 最后登录时间（可选）,使用指针可为null
+
+	RoleID uint `gorm:"not null"` // 关联的角色ID
+	Role   role.Role
+}
+
+// 接收明文密码并使用bcrypt哈希化存储
+func (u *User) SetPassword(password string) error {
+	PasswordHash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return fmt.Errorf("failed to generate password hash: %w", err)
+	}
+	u.PasswordHash = string(PasswordHash)
+	return nil
+}
+
+// 验证明文密码与存储的哈希值是否匹配
+func (u *User) CheckPassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.PasswordHash), []byte(password))
+	return err == nil // 如果 err 为 nil，表示密码匹配
 }
