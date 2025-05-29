2c0a144bfdb6b7e3a630047939934c5d7af95351
peng <peng.nullptr@qq.com>
2025-05-30 02:52:14 +0800

    feat(request): 添加用户注册请求结构体 RegisterUserRequest

---
+++ internal/api/dto/request/user_request.go
@@ -0,0 +1,9 @@
+package request
+
+// RegisterUserRequest 定义了用户注册请求的结构体。
+type RegisterUserRequest struct {
+	Username    string `json:"username" binding:"required,alphanum,min=3,max=50"`
+	Password    string `json:"password" binding:"required,min=8,max=100"`
+	Email       string `json:"email" binding:"required,email"`
+	DefaultRole string `json:"default_role" binding:"omitempty,alphanum"` // 可选，如果允许客户端指定
+}
