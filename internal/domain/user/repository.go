c728408f4efabbc19cee4e62cd15cd7490c17428
peng <peng.nullptr@qq.com>
2025-05-29 18:02:29 +0800

    feat(user): 更新用户存储库接口，修正通过邮箱获取用户的注释

--- internal/domain/user/repository.go
+++ internal/domain/user/repository.go
@@ -9,7 +9,7 @@ type UserRepository interface {
 	Create(ctx context.Context, user *User) error                       // 创建用户
 	FindByID(ctx context.Context, id uint) (*User, error)               // 通过ID获取用户
 	FindByUsername(ctx context.Context, username string) (*User, error) // 通过name获取用户
-	FindByEmail(ctx context.Context, email string) (*User, error)       // 通过name获取用户
+	FindByEmail(ctx context.Context, email string) (*User, error)       // 通过email获取用户
 	Update(ctx context.Context, user *User) error                       // 更新用户
 	Delete(ctx context.Context, id uint) error                          // 删除用户
 }
