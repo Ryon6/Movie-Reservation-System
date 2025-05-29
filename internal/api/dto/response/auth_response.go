5a50348dc126d3b34b3c7c043ddf2c9bf2f28518
peng <peng.nullptr@qq.com>
2025-05-30 02:56:32 +0800

    feat(response): 添加成功登录后的响应结构体 LoginResponse

---
+++ internal/api/dto/response/auth_response.go
@@ -0,0 +1,10 @@
+package response
+
+import "time"
+
+// LoginResponse 定义了成功登录后返回的结构体。
+type LoginResponse struct {
+	Token     string      `json:"token"`
+	ExpiresAt time.Time   `json:"expires_at"`
+	User      UserReponse `json:"user"`
+}
