0be5fb541f26036e1f43a2f7f10a0ff13a86d71b
peng <peng.nullptr@qq.com>
2025-05-30 17:51:45 +0800

    feat(user): 使用自定义密码哈希器更新 SetPassword 方法，移除 bcrypt 依赖

--- internal/domain/user/user.go
+++ internal/domain/user/user.go
@@ -3,9 +3,9 @@ package user
 import (
 	"fmt"
 	"mrs/internal/domain/role"
+	"mrs/internal/utils"
 	"time"
 
-	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
@@ -22,8 +22,8 @@ type User struct {
 }
 
 // 接收明文密码并使用bcrypt哈希化存储
-func (u *User) SetPassword(password string) error {
-	PasswordHash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+func (u *User) SetPassword(password string, hasher utils.PasswordHasher) error {
+	PasswordHash, err := hasher.Hash(password)
 	if err != nil {
 		return fmt.Errorf("failed to generate password hash: %w", err)
 	}
@@ -31,8 +31,12 @@ func (u *User) SetPassword(password string) error {
 	return nil
 }
 
-// 验证明文密码与存储的哈希值是否匹配
-func (u *User) CheckPassword(password string) bool {
-	err := bcrypt.CompareHashAndPassword([]byte(u.PasswordHash), []byte(password))
-	return err == nil // 如果 err 为 nil，表示密码匹配
-}
+// // 验证明文密码与存储的哈希值是否匹配
+// func (u *User) CheckPassword(password string, hasher utils.PasswordHasher) bool {
+// 	// err := bcrypt.CompareHashAndPassword([]byte(u.PasswordHash), []byte(password))
+// 	ok, err := hasher.Check(u.PasswordHash, password)
+// 	if err != nil {
+// 		return false
+// 	}
+// 	return ok // 如果 err 为 nil，表示密码匹配
+// }
