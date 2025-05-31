70adfd1bf60030f89f5352c4d32e4fb625f1f0e1
peng <peng.nullptr@qq.com>
2025-06-01 01:38:24 +0800

    fix(repository): 修正类型定义和返回值，确保接口一致性

--- internal/domain/movie/genre_repository.go
+++ internal/domain/movie/genre_repository.go
@@ -7,9 +7,9 @@ type GenreRepository interface {
 	Create(ctx context.Context, genre *Genre) error
 	FindByID(ctx context.Context, id uint) (*Genre, error)
 	FindByName(ctx context.Context, name string) (*Genre, error)
-	ListAll(ctx context.Context) (*[]Genre, error)
+	ListAll(ctx context.Context) ([]*Genre, error)
 	Update(ctx context.Context, genre *Genre) error
-	Delete(ctx context.Context, id uint) (*Genre, error)
+	Delete(ctx context.Context, id uint) error
 	// 常用：查找或创建
 	FindOrCreateByName(ctx context.Context, name string) (*Genre, error)
 }
--- internal/domain/movie/movie_repository.go
+++ internal/domain/movie/movie_repository.go
@@ -3,14 +3,14 @@ package movie
 import "context"
 
 // MovieRepository 定义了电影实体的持久化操作接口。
-type MovieResritory interface {
+type MovieRepository interface {
 	Create(ctx context.Context, movie *Movie) error
 	// 应支持预加载Genres
 	FindByID(ctx context.Context, id uint) (*Movie, error)
 	// 分页和过滤，返回总数
 	List(ctx context.Context, page, pageSize int, filters map[string]interface{}) ([]*Movie, int64, error)
-	Update(ctx context.Context, genre *Genre) error
-	Delete(ctx context.Context, id uint) (*Genre, error)
+	Update(ctx context.Context, movie *Movie) error
+	Delete(ctx context.Context, id uint) error
 	// 为电影增加、删除和修改类型
 	AddGenreToMovie(ctx context.Context, movie *Movie, genre *Genre) error
 	RemoveGenreToMovie(ctx context.Context, movie *Movie, genre *Genre) error
