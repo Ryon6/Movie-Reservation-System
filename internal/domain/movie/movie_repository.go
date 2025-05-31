074f89c587d4ada31deef9c809d4f7a52d75adcb
peng <peng.nullptr@qq.com>
2025-06-01 01:21:58 +0800

    feat(movie): 添加电影实体持久化操作接口，支持电影信息管理

---
+++ internal/domain/movie/movie_repository.go
@@ -0,0 +1,18 @@
+package movie
+
+import "context"
+
+// MovieRepository 定义了电影实体的持久化操作接口。
+type MovieResritory interface {
+	Create(ctx context.Context, movie *Movie) error
+	// 应支持预加载Genres
+	FindByID(ctx context.Context, id uint) (*Movie, error)
+	// 分页和过滤，返回总数
+	List(ctx context.Context, page, pageSize int, filters map[string]interface{}) ([]*Movie, int64, error)
+	Update(ctx context.Context, genre *Genre) error
+	Delete(ctx context.Context, id uint) (*Genre, error)
+	// 为电影增加、删除和修改类型
+	AddGenreToMovie(ctx context.Context, movie *Movie, genre *Genre) error
+	RemoveGenreToMovie(ctx context.Context, movie *Movie, genre *Genre) error
+	ReplaceGenreForMovie(ctx context.Context, movie *Movie, genre []*Genre) error
+}
