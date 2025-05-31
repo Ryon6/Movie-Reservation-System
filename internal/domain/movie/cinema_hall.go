bc4544085670b9efacc47d4592338e2ae42d6b30
peng <peng.nullptr@qq.com>
2025-05-31 18:29:34 +0800

    feat(cinema_hall): 添加影厅结构体及其字段定义，支持影厅信息管理

---
+++ internal/domain/movie/cinema_hall.go
@@ -0,0 +1,14 @@
+package movie
+
+import "gorm.io/gorm"
+
+type CinemaHall struct {
+	gorm.Model
+	Name        string `gorm:"type:varchar(50);uniqueIndex;not null;"` // 影厅名称 (例如: "1号厅", "IMAX厅")。
+	ScreenType  string `gorm:"type:varchar(50)"`                       // 屏幕类型，如 "2D", "3D", "IMAX"
+	SoundSystem string `gorm:"type:varchar(100)"`                      // 音响系统
+	Capacity    int    // 总座位数
+
+	Seats     []Seat     `gorm:"foreignKey:CinemaHallID"`
+	Showtimes []Showtime `gorm:"foreignKey:CinemaHallID"`
+}
