ae51cce21eabeaa4fc3315d7d67a2469eb33937f
peng <peng.nullptr@qq.com>
2025-05-31 19:02:27 +0800

    feat(showtime): 更新放映时间结构体，添加联合索引以优化查询性能

--- internal/domain/movie/showtime.go
+++ internal/domain/movie/showtime.go
@@ -8,12 +8,19 @@ import (
 
 type Showtime struct {
 	gorm.Model
-	MovieID      uint       `gorm:"not null;index"`          // 关联的电影ID 已在Movie中定义外键
-	Movie        Movie      `gorm:"foreignKey:MovieID"`      // 关联的电影实体
-	CinemaHallID uint       `gorm:"not null;index"`          // 关联的影厅ID 已在CinemaHall中定义外键
-	CinemaHall   CinemaHall `gorm:"foreignKey:CinemaHallID"` // 关联的影厅实体
+	// 关联的电影ID 已在Movie中定义外键 电影ID，联合索引1
+	MovieID uint `gorm:"not null;index;index:idx_movie_start_time,priority:1"`
+	// 关联的电影实体
+	Movie Movie `gorm:"foreignKey:MovieID"`
+	// 关联的影厅ID 已在CinemaHall中定义外键 影厅ID，联合索引2
+	CinemaHallID uint `gorm:"not null;index;index:idx_hall_start_time,priority:1"`
+	// 关联的影厅实体
+	CinemaHall CinemaHall `gorm:"foreignKey:CinemaHallID"`
 
-	StartTime time.Time `gorm:"not null;index"` // 放映开始时间 (包含日期和时间)
-	EndTime   time.Time `gorm:"not null"`       // 放映结束时间 (可以根据电影时长和开始时间计算)
-	Price     float64   `gorm:"not null"`       // 该场次的票价 (可以更复杂，比如不同座位类型不同价格)
+	// 放映开始时间 (包含日期和时间) 放映开始时间，联合索引1和2
+	StartTime time.Time `gorm:"not null;index;index:idx_movie_start_time,priority:2;index:idx_hall_start_time,priority:2"`
+	// 放映结束时间 (可以根据电影时长和开始时间计算)
+	EndTime time.Time `gorm:"not null"`
+	// 该场次的票价 (可以更复杂，比如不同座位类型不同价格)
+	Price float64 `gorm:"not null"`
 }
