e99586d0b66ff4eec35bd58ed15a9159a2f49e6b
peng <peng.nullptr@qq.com>
2025-06-04 07:06:09 +0800

    feat(genre): 增加类型Gorm模型与领域模型之间的转换方法，优化类型数据的处理方式

--- internal/infrastructure/persistence/mysql/models/genre_grom.go
+++ internal/infrastructure/persistence/mysql/models/genre_grom.go
@@ -1,9 +1,33 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"mrs/internal/domain/movie"
+	"mrs/internal/domain/shared/vo"
+
+	"gorm.io/gorm"
+)
 
 type GenreGrom struct {
 	gorm.Model
 	Name   string       `gorm:"type:varchar(100);uniqueIndex;not null"`              // 类型名称：科幻...
 	Movies []*MovieGrom `gorm:"many2many:movie_genres;constraint:OnDelete:RESTRICT"` // 该类型下的电影 (可选，如果需要反向查询)
+}
+
+func (g *GenreGrom) ToDomain() *movie.Genre {
+	return &movie.Genre{
+		ID:   vo.GenreID(g.ID),
+		Name: g.Name,
+	}
+}
+
+func GenreGromFromDomain(g *movie.Genre) *GenreGrom {
+	movies := make([]*MovieGrom, len(g.Movies))
+	for i, movie := range g.Movies {
+		movies[i] = MovieGromFromDomain(movie)
+	}
+	return &GenreGrom{
+		Model:  gorm.Model{ID: uint(g.ID)},
+		Name:   g.Name,
+		Movies: movies,
+	}
 }
