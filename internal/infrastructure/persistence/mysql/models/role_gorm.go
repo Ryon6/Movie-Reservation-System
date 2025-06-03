6755ed8d6cefbf0ab41d9c15be8d818822cd8e7d
peng <peng.nullptr@qq.com>
2025-06-04 07:05:21 +0800

    feat(role): 增加角色Gorm模型与领域模型之间的转换方法，优化角色数据的处理方式

--- internal/infrastructure/persistence/mysql/models/role_gorm.go
+++ internal/infrastructure/persistence/mysql/models/role_gorm.go
@@ -1,9 +1,30 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"mrs/internal/domain/shared/vo"
+	"mrs/internal/domain/user"
+
+	"gorm.io/gorm"
+)
 
 type RoleGorm struct {
 	gorm.Model
 	Name        string `gorm:"type:varchar(50);uniqueIndex;not null"`
 	Description string `gorm:"type:varchar(255)"` // 角色描述（可选）
+}
+
+func (r *RoleGorm) ToDomain() *user.Role {
+	return &user.Role{
+		ID:          vo.RoleID(r.ID),
+		Name:        r.Name,
+		Description: r.Description,
+	}
+}
+
+func RoleGormFromDomain(r *user.Role) *RoleGorm {
+	return &RoleGorm{
+		Model:       gorm.Model{ID: uint(r.ID)},
+		Name:        r.Name,
+		Description: r.Description,
+	}
 }
