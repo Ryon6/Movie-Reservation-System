8c285a013af1dc28d3e0f97f8ad693dcf85e2a8a
peng <peng.nullptr@qq.com>
2025-06-04 06:31:31 +0800

    feat(seat): 更新座位模型，使用值对象替代GORM模型，优化座位属性定义

--- internal/domain/cinema/seat.go
+++ internal/domain/cinema/seat.go
@@ -1,6 +1,6 @@
 package cinema
 
-import "gorm.io/gorm"
+import "mrs/internal/domain/shared/vo"
 
 type SeatType string
 
@@ -12,11 +12,10 @@ const (
 
 // 注意：座位的可用性通常与特定的Showtime相关，而不是座位本身的静态属性。
 type Seat struct {
-	gorm.Model
+	ID vo.SeatID
 	// 对 CinemaHallID 单独建立索引: 应对高频查询影厅全部座位的需求
-	CinemaHallID uint       `gorm:"not null;index;uniqueIndex:idx_hall_row_number"` // 联合唯一索引
-	CinemaHall   CinemaHall `gorm:"foreignKey:CinemaHallID"`
-	Row          string     `gorm:"type:varchar(10);not null;uniqueIndex:idx_hall_row_number"` // 联合唯一索引
-	RowNumber    string     `gorm:"type:varchar(10);not null;uniqueIndex:idx_hall_row_number"`
-	Type         SeatType   `gorm:"type:varchar(50);default:'STANDARD'"`
+	CinemaHallID vo.CinemaHallID
+	Row          string
+	RowNumber    string
+	Type         SeatType
 }
