6ffc088d4c04865cf357c578c43f6e674c83fbe7
peng <peng.nullptr@qq.com>
2025-06-04 06:22:58 +0800

    feat(identifier): 新增标识符值对象，定义用户ID、角色ID、电影ID、类型ID、影厅ID、场次ID和座位ID

---
+++ internal/domain/shared/vo/identifier.go
@@ -0,0 +1,16 @@
+// value object(vo) 值对象
+package vo
+
+type UserID uint
+
+type RoleID uint
+
+type MovieID uint
+
+type GenreID uint
+
+type CinemaHallID uint
+
+type ShowtimeID uint
+
+type SeatID uint
