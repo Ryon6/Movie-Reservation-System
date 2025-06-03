aae2067edd298b7e37495d255e8b0f1eb65c5ba2
peng <peng.nullptr@qq.com>
2025-06-04 07:05:50 +0800

    feat(seat): 增加座位Gorm模型与领域模型之间的转换方法，优化座位数据的处理方式

--- internal/infrastructure/persistence/mysql/models/seat_gorm.go
+++ internal/infrastructure/persistence/mysql/models/seat_gorm.go
@@ -1,6 +1,11 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"mrs/internal/domain/cinema"
+	"mrs/internal/domain/shared/vo"
+
+	"gorm.io/gorm"
+)
 
 type SeatGrom struct {
 	gorm.Model
@@ -10,4 +15,25 @@ type SeatGrom struct {
 	Row          string         `gorm:"type:varchar(10);not null;uniqueIndex:idx_hall_row_number"` // 联合唯一索引
 	RowNumber    string         `gorm:"type:varchar(10);not null;uniqueIndex:idx_hall_row_number"`
 	Type         string         `gorm:"type:varchar(50);default:'STANDARD'"`
+}
+
+func (s *SeatGrom) ToDomain() *cinema.Seat {
+	return &cinema.Seat{
+		ID:         vo.SeatID(s.ID),
+		CinemaHall: s.CinemaHall.ToDomain(),
+		Row:        s.Row,
+		RowNumber:  s.RowNumber,
+		Type:       cinema.SeatType(s.Type),
+	}
+}
+
+func SeatGromFromDomain(s *cinema.Seat) *SeatGrom {
+	return &SeatGrom{
+		Model:        gorm.Model{ID: uint(s.ID)},
+		CinemaHallID: uint(s.CinemaHall.ID),
+		CinemaHall:   *CinemaHallGromFromDomain(s.CinemaHall),
+		Row:          s.Row,
+		RowNumber:    s.RowNumber,
+		Type:         string(s.Type),
+	}
 }
