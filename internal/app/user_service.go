2b8675c24e0a8c3a95c1e977be555b8428125d80
peng <peng.nullptr@qq.com>
2025-06-04 21:28:31 +0800

    feat(user): 重构用户服务，更新返回类型为用户模型，优化错误处理逻辑

--- internal/app/user_service.go
+++ internal/app/user_service.go
@@ -4,52 +4,30 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"mrs/internal/domain/role"
 	"mrs/internal/domain/user"
 	"mrs/internal/utils"
 	applog "mrs/pkg/log"
-	"time"
 
 	"gorm.io/gorm"
 )
 
 type UserService interface {
-	RegisterUser(ctx context.Context, username, email, plainPassword, defaultRoleName string) (*UserProfile, error)
-	GetUserByID(ctx context.Context, userID uint) (*UserProfile, error)
+	RegisterUser(ctx context.Context, username, email, plainPassword, defaultRoleName string) (*user.User, error)
+	GetUserByID(ctx context.Context, userID uint) (*user.User, error)
 }
 
 type userService struct {
 	defaultRoleName string // 注入配置依赖：默认角色
 	userRepo        user.UserRepository
-	roleRepo        role.RoleRepository
+	roleRepo        user.RoleRepository
 	hasher          utils.PasswordHasher
 	logger          applog.Logger
-}
-
-type UserProfile struct {
-	ID       uint      `json:"id"`
-	Username string    `json:"username"`
-	Email    string    `json:"email"`
-	RoleName string    `json:"role_name"`
-	CreateAt time.Time `json:"create_at"`
-	UpdateAt time.Time `json:"update_at"`
-}
-
-func toUserProfile(usr *user.User) *UserProfile {
-	return &UserProfile{
-		ID:       usr.ID,
-		Username: usr.Username,
-		Email:    usr.Email,
-		RoleName: usr.Role.Name,
-		CreateAt: usr.CreatedAt,
-		UpdateAt: usr.UpdatedAt,
-	}
 }
 
 func NewUserService(
 	defaultRoleName string,
 	userRepo user.UserRepository,
-	roleRepo role.RoleRepository,
+	roleRepo user.RoleRepository,
 	hasher utils.PasswordHasher,
 	logger applog.Logger,
 ) UserService {
@@ -58,12 +36,12 @@ func NewUserService(
 		userRepo:        userRepo,
 		roleRepo:        roleRepo,
 		hasher:          hasher,
-		logger:          logger,
+		logger:          logger.With(applog.String("Service", "UserService")),
 	}
 }
 
-func (s *userService) RegisterUser(ctx context.Context, username, email, plainPassword, defaultRoleName string) (*UserProfile, error) {
-	logger := s.logger.With(applog.String("Method", "userService.RegisterUser"),
+func (s *userService) RegisterUser(ctx context.Context, username, email, plainPassword, defaultRoleName string) (*user.User, error) {
+	logger := s.logger.With(applog.String("Method", "RegisterUser"),
 		applog.String("username", username),
 		applog.String("email", email))
 	// 验证用户名是否存在
@@ -71,7 +49,7 @@ func (s *userService) RegisterUser(ctx context.Context, username, email, plainPa
 	if err == nil {
 		// 如果err为nil说明用户名已存在
 		logger.Warn("username already exists")
-		return nil, errors.New("username already exists")
+		return nil, fmt.Errorf("%w: %w", user.ErrUserAlreadyExists, err)
 	} else if !errors.Is(err, gorm.ErrRecordNotFound) {
 		logger.Error("failed to check username existence", applog.Error(err))
 		return nil, fmt.Errorf("userService.RegisterUser: %w", err)
@@ -81,7 +59,7 @@ func (s *userService) RegisterUser(ctx context.Context, username, email, plainPa
 	_, err = s.userRepo.FindByEmail(ctx, email)
 	if err == nil {
 		logger.Warn("email already exists")
-		return nil, errors.New("email already exists")
+		return nil, fmt.Errorf("%w: %w", user.ErrUserAlreadyExists, err)
 	} else if !errors.Is(err, gorm.ErrRecordNotFound) {
 		logger.Error("failed to check email existence", applog.Error(err))
 		return nil, fmt.Errorf("userService.RegisterUser: %w", err)
@@ -95,8 +73,7 @@ func (s *userService) RegisterUser(ctx context.Context, username, email, plainPa
 	if err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			logger.Error("Default role not found", applog.String("role_name", defaultRoleName))
-			// return nil, domainErrors.NewNotFoundError("default role '%s' not found", defaultRoleName)
-			return nil, errors.New("default role not found")
+			return nil, fmt.Errorf("default role not found: %w", user.ErrRoleNotFound)
 		}
 		logger.Error("Failed to find default role", applog.Error(err))
 		return nil, err
@@ -106,13 +83,12 @@ func (s *userService) RegisterUser(ctx context.Context, username, email, plainPa
 	newUser := user.User{
 		Username: username,
 		Email:    email,
-		RoleID:   defaultRole.ID,
-		Role:     *defaultRole,
+		Role:     defaultRole,
 	}
 
 	if err := newUser.SetPassword(plainPassword, s.hasher); err != nil {
 		logger.Error("failed to hash password", applog.Error(err))
-		return nil, errors.New("failed to process password")
+		return nil, fmt.Errorf("%w: %w", user.ErrInvalidPassword, err)
 	}
 
 	// 保存用户
@@ -122,21 +98,21 @@ func (s *userService) RegisterUser(ctx context.Context, username, email, plainPa
 		return nil, fmt.Errorf("userService.RegisterUser: %w", err)
 	}
 	logger.Info("create user successful")
-	return toUserProfile(&newUser), nil
+	return &newUser, nil
 }
 
-func (s *userService) GetUserByID(ctx context.Context, userID uint) (*UserProfile, error) {
-	logger := s.logger.With(applog.String("Method", "userService.GetUserByID"), applog.Uint("user_id", userID))
+func (s *userService) GetUserByID(ctx context.Context, userID uint) (*user.User, error) {
+	logger := s.logger.With(applog.String("Method", "GetUserByID"), applog.Uint("user_id", userID))
 	usr, err := s.userRepo.FindByID(ctx, userID)
 	if err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			logger.Warn("user not found")
-			return nil, errors.New("user not found")
+			return nil, fmt.Errorf("%w: %w", user.ErrUserNotFound, err)
 		}
 		logger.Error("failed to find user by ID", applog.Error(err))
 		return nil, fmt.Errorf("userService.GetUserByID: %w", err)
 	}
 
 	logger.Info("find user successfully")
-	return toUserProfile(usr), nil
+	return usr, nil
 }
