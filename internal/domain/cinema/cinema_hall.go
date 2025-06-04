5031042ede7e4a4e8660744c33dffea68fce7b2d
peng <peng.nullptr@qq.com>
2025-06-04 08:29:54 +0800

    feat(cinema_hall): 移除影厅模型中的场次引用，优化聚合内部实体的引用方式

--- internal/domain/cinema/cinema_hall.go
+++ internal/domain/cinema/cinema_hall.go
@@ -2,7 +2,6 @@ package cinema
 
 import (
 	"mrs/internal/domain/shared/vo"
-	"mrs/internal/domain/showtime"
 )
 
 // 影厅
@@ -14,6 +13,5 @@ type CinemaHall struct {
 	Capacity    int             // 总座位数
 
 	// 多对多关系
-	Seats     []*Seat              // 聚合内部可以直接持有同一聚合内其他实体的引用
-	Showtimes []*showtime.Showtime // 跨聚合关系优先使用ID，根据业务需求选择持有引用或ID
+	Seats []*Seat // 聚合内部可以直接持有同一聚合内其他实体的引用
 }
