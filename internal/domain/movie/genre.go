f7354760cd03d3134f92953f1eccb39e1cc55ab7
peng <peng.nullptr@qq.com>
2025-06-05 05:23:42 +0800

    refactor(genre): 移除Genre结构中的Movies字段，更新GenreGrom结构以简化多对多关系定义

--- internal/domain/movie/genre.go
+++ internal/domain/movie/genre.go
@@ -6,7 +6,4 @@ import "mrs/internal/domain/shared/vo"
 type Genre struct {
 	ID   vo.GenreID // 类型ID
 	Name string     // 类型名称
-
-	// 多对多关系
-	Movies []*Movie
 }
--- internal/infrastructure/persistence/mysql/models/genre_grom.go
+++ internal/infrastructure/persistence/mysql/models/genre_grom.go
@@ -9,8 +9,8 @@ import (
 
 type GenreGrom struct {
 	gorm.Model
-	Name   string       `gorm:"type:varchar(100);uniqueIndex;not null"`              // 类型名称：科幻...
-	Movies []*MovieGrom `gorm:"many2many:movie_genres;constraint:OnDelete:RESTRICT"` // 该类型下的电影 (可选，如果需要反向查询)
+	Name string `gorm:"type:varchar(100);uniqueIndex;not null"` // 类型名称：科幻...
+	// 只需一方显式定义多对多关系，另一方会自动创建连接表
 }
 
 func (g *GenreGrom) ToDomain() *movie.Genre {
@@ -21,13 +21,8 @@ func (g *GenreGrom) ToDomain() *movie.Genre {
 }
 
 func GenreGromFromDomain(g *movie.Genre) *GenreGrom {
-	movies := make([]*MovieGrom, len(g.Movies))
-	for i, movie := range g.Movies {
-		movies[i] = MovieGromFromDomain(movie)
-	}
 	return &GenreGrom{
-		Model:  gorm.Model{ID: uint(g.ID)},
-		Name:   g.Name,
-		Movies: movies,
+		Model: gorm.Model{ID: uint(g.ID)},
+		Name:  g.Name,
 	}
 }
