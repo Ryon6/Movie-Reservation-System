569ef7a0918de6ef543dccbe9d2d0bcede7942e5
peng <peng.nullptr@qq.com>
2025-06-04 09:36:04 +0800

    feat(seat): 重构座位存储库，使用座位Gorm模型与领域模型之间的转换，优化座位数据的处理方式

--- internal/infrastructure/persistence/mysql/seat_repository.go
+++ internal/infrastructure/persistence/mysql/seat_repository.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"mrs/internal/domain/movie"
+	"mrs/internal/domain/cinema"
+	"mrs/internal/domain/shared"
+	"mrs/internal/infrastructure/persistence/mysql/models"
 	applog "mrs/pkg/log"
 
 	"gorm.io/gorm"
@@ -15,17 +17,21 @@ type gormSeatRepository struct {
 	db     *gorm.DB
 }
 
-func NewGormSeatRepository(db *gorm.DB, logger applog.Logger) movie.SeatRepository {
+func NewGormSeatRepository(db *gorm.DB, logger applog.Logger) cinema.SeatRepository {
 	return &gormSeatRepository{
-		logger: logger,
+		logger: logger.With(applog.String("Repository", "gormSeatRepository")),
 		db:     db,
 	}
 }
 
-func (r *gormSeatRepository) CreateBatch(ctx context.Context, seats []*movie.Seat) error {
-	logger := r.logger.With(applog.String("Method", "gormSeatRepository.CreateBatch"), applog.Int("seat count", len(seats)))
+func (r *gormSeatRepository) CreateBatch(ctx context.Context, seats []*cinema.Seat) error {
+	logger := r.logger.With(applog.String("Method", "CreateBatch"), applog.Int("seat count", len(seats)))
 
-	if err := r.db.WithContext(ctx).Create(&seats).Error; err != nil {
+	seatGorms := make([]*models.SeatGrom, len(seats))
+	for i, seat := range seats {
+		seatGorms[i] = models.SeatGromFromDomain(seat)
+	}
+	if err := r.db.WithContext(ctx).Create(&seatGorms).Error; err != nil {
 		logger.Error("failed to create seats", applog.Error(err))
 		return err
 	}
@@ -34,69 +40,78 @@ func (r *gormSeatRepository) CreateBatch(ctx context.Context, seats []*movie.Sea
 	return nil
 }
 
-func (r *gormSeatRepository) FindByID(ctx context.Context, id uint) (*movie.Seat, error) {
-	logger := r.logger.With(applog.String("Method", "gormSeatRepository.FindByID"), applog.Uint("seat_id", id))
-	var seat movie.Seat
-	if err := r.db.WithContext(ctx).First(&seat, id).Error; err != nil {
+func (r *gormSeatRepository) FindByID(ctx context.Context, id uint) (*cinema.Seat, error) {
+	logger := r.logger.With(applog.String("Method", "FindByID"), applog.Uint("seat_id", id))
+	var seatGorm models.SeatGrom
+	if err := r.db.WithContext(ctx).First(&seatGorm, id).Error; err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			logger.Warn("seat id not found", applog.Error(err))
-			return nil, fmt.Errorf("%w(id): %w", movie.ErrSeatNotFound, err)
+			return nil, fmt.Errorf("%w(id): %w", cinema.ErrSeatNotFound, err)
 		}
 		logger.Error("failed to find seat by id", applog.Error(err))
 		return nil, fmt.Errorf("failed to find seat by id: %w", err)
 	}
 
 	logger.Info("find seat by id successfully")
-	return &seat, nil
+	return seatGorm.ToDomain(), nil
 }
 
-func (r *gormSeatRepository) FindByHallID(ctx context.Context, hallID uint) ([]*movie.Seat, error) {
-	logger := r.logger.With(applog.String("Method", "gormSeatRepository.FindByHallID"), applog.Uint("hall_id", hallID))
-	var seats []*movie.Seat
+func (r *gormSeatRepository) FindByHallID(ctx context.Context, hallID uint) ([]*cinema.Seat, error) {
+	logger := r.logger.With(applog.String("Method", "FindByHallID"), applog.Uint("hall_id", hallID))
+	var seatsGorms []*models.SeatGrom
 
-	if err := r.db.WithContext(ctx).Where("cinema_hall_id = ?", hallID).Find(&seats).Error; err != nil {
+	if err := r.db.WithContext(ctx).Where("cinema_hall_id = ?", hallID).Find(&seatsGorms).Error; err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			logger.Warn("no seats found for hall", applog.Error(err))
-			return nil, fmt.Errorf("%w: %w", movie.ErrSeatNotFound, err)
+			return nil, fmt.Errorf("%w: %w", cinema.ErrCinemaHallNotFound, err)
 		}
 		logger.Error("failed to find seats by hall id", applog.Error(err))
 		return nil, fmt.Errorf("failed to find seats by hall id: %w", err)
 	}
 
 	logger.Info("find seats by hall id successfully")
+	seats := make([]*cinema.Seat, len(seatsGorms))
+	for i, seatGorm := range seatsGorms {
+		seats[i] = seatGorm.ToDomain()
+	}
 	return seats, nil
 }
 
-func (r *gormSeatRepository) GetSeatsByIDs(ctx context.Context, seatIDs []uint) ([]*movie.Seat, error) {
-	logger := r.logger.With(applog.String("Method", "gormSeatRepository.GetSeatsByIDs"), applog.Int("seat count", len(seatIDs)))
-	var seats []*movie.Seat
-	if err := r.db.WithContext(ctx).Where("id IN ?", seatIDs).Find(&seats).Error; err != nil {
+func (r *gormSeatRepository) GetSeatsByIDs(ctx context.Context, seatIDs []uint) ([]*cinema.Seat, error) {
+	logger := r.logger.With(applog.String("Method", "GetSeatsByIDs"), applog.Int("seat count", len(seatIDs)))
+	var seatsGorms []*models.SeatGrom
+	if err := r.db.WithContext(ctx).Where("id IN ?", seatIDs).Find(&seatsGorms).Error; err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			logger.Warn("seats not found", applog.Error(err))
-			return nil, fmt.Errorf("%w: %w", movie.ErrSeatNotFound, err)
+			return nil, fmt.Errorf("%w: %w", cinema.ErrSeatNotFound, err)
 		}
 		logger.Error("failed to get seats by ids", applog.Error(err))
 		return nil, fmt.Errorf("failed to get seats by ids: %w", err)
 	}
 
 	logger.Info("get seats by ids successfully")
+	seats := make([]*cinema.Seat, len(seatsGorms))
+	for i, seatGorm := range seatsGorms {
+		seats[i] = seatGorm.ToDomain()
+	}
 	return seats, nil
 }
 
-func (r *gormSeatRepository) Update(ctx context.Context, seat *movie.Seat) error {
-	logger := r.logger.With(applog.String("Method", "gormSeatRepository.Update"),
-		applog.Uint("seat_id", seat.ID), applog.Uint("hall_id", seat.CinemaHallID))
+func (r *gormSeatRepository) Update(ctx context.Context, seat *cinema.Seat) error {
+	logger := r.logger.With(applog.String("Method", "Update"),
+		applog.Uint("seat_id", uint(seat.ID)), applog.Uint("hall_id", uint(seat.CinemaHall.ID)))
 
-	if err := r.db.WithContext(ctx).First(&movie.Seat{}, seat.ID).Error; err != nil {
+	seatGorm := models.SeatGromFromDomain(seat)
+	if err := r.db.WithContext(ctx).First(&models.SeatGrom{}, seatGorm.ID).Error; err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			logger.Warn("seat not found", applog.Error(err))
-			return fmt.Errorf("%w: %w", movie.ErrSeatNotFound, err)
+			return fmt.Errorf("%w: %w", cinema.ErrSeatNotFound, err)
 		}
 		logger.Error("failed to find seat by id", applog.Error(err))
 		return fmt.Errorf("failed to find seat by id: %w", err)
 	}
 
-	result := r.db.WithContext(ctx).Where("id = ?", seat.ID).Updates(&seat)
+	result := r.db.WithContext(ctx).Where("id = ?", seatGorm.ID).Updates(&seatGorm)
 	if err := result.Error; err != nil {
 		logger.Error("failed to update seat", applog.Error(err))
 		return fmt.Errorf("failed to update seat: %w", err)
@@ -104,7 +119,7 @@ func (r *gormSeatRepository) Update(ctx context.Context, seat *movie.Seat) error
 
 	if result.RowsAffected == 0 {
 		logger.Warn("no rows affected during update")
-		return movie.ErrNoRowsAffected
+		return shared.ErrNoRowsAffected
 	}
 
 	logger.Info("update seat successfully")
@@ -112,9 +127,9 @@ func (r *gormSeatRepository) Update(ctx context.Context, seat *movie.Seat) error
 }
 
 func (r *gormSeatRepository) Delete(ctx context.Context, id uint) error {
-	logger := r.logger.With(applog.String("Method", "gormSeatRepository.Delete"), applog.Uint("seat_id", id))
+	logger := r.logger.With(applog.String("Method", "Delete"), applog.Uint("seat_id", id))
 
-	result := r.db.WithContext(ctx).Delete(&movie.Seat{}, id)
+	result := r.db.WithContext(ctx).Delete(&models.SeatGrom{}, id)
 	if err := result.Error; err != nil {
 		logger.Error("failed to delete seat", applog.Error(err))
 		return fmt.Errorf("failed to delete seat: %w", err)
@@ -122,7 +137,7 @@ func (r *gormSeatRepository) Delete(ctx context.Context, id uint) error {
 	// 是否不存在或已删除
 	if result.RowsAffected == 0 {
 		logger.Warn("seat to delete not found or already deleted")
-		return movie.ErrSeatNotFound
+		return shared.ErrNoRowsAffected
 	}
 
 	logger.Info("delete seat successfully")
@@ -130,9 +145,9 @@ func (r *gormSeatRepository) Delete(ctx context.Context, id uint) error {
 }
 
 func (r *gormSeatRepository) DeleteByHallID(ctx context.Context, hallID uint) error {
-	logger := r.logger.With(applog.String("Method", "gormSeatRepository.DeleteByHallID"), applog.Uint("hall_id", hallID))
+	logger := r.logger.With(applog.String("Method", "DeleteByHallID"), applog.Uint("hall_id", hallID))
 
-	result := r.db.WithContext(ctx).Where("cinema_hall_id = ?", hallID).Delete(&movie.Seat{})
+	result := r.db.WithContext(ctx).Where("cinema_hall_id = ?", hallID).Delete(&models.SeatGrom{})
 	if err := result.Error; err != nil {
 		logger.Error("failed to delete seats", applog.Error(err))
 		return fmt.Errorf("failed to delete seats: %w", err)
@@ -140,7 +155,7 @@ func (r *gormSeatRepository) DeleteByHallID(ctx context.Context, hallID uint) er
 
 	if result.RowsAffected == 0 {
 		logger.Warn("no seats found to delete")
-		return movie.ErrSeatNotFound
+		return shared.ErrNoRowsAffected
 	}
 
 	logger.Info("delete seats by hall id successfully")
