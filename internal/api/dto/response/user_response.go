9efa5922c60b3a77608c7eea1c4597c1655a6a71
peng <peng.nullptr@qq.com>
2025-06-04 21:31:44 +0800

    feat(user_response): 移除用户响应结构中的CreateAt和UpdateAt字段，简化用户信息返回结构

--- internal/api/dto/response/user_response.go
+++ internal/api/dto/response/user_response.go
@@ -1,15 +1,11 @@
 package response
 
-import (
-	"time"
-)
-
 type UserResponse struct {
-	ID       uint      `json:"id"`
-	Username string    `json:"username"`
-	Email    string    `json:"email"`
-	RoleName string    `json:"role_name"` // 来自关联的 Role 实体的 Name 字段
-	CreateAt time.Time `json:"create_at"`
-	UpdateAt time.Time `json:"update_at"`
+	ID       uint   `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	RoleName string `json:"role_name"` // 来自关联的 Role 实体的 Name 字段
+	// CreateAt time.Time `json:"create_at"`
+	// UpdateAt time.Time `json:"update_at"`
 	// IsActive bool      `json:"is_active"`
 }
