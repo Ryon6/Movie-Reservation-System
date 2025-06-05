038c5f76d9e6e39526b664206478a7dd782f37ca
peng <peng.nullptr@qq.com>
2025-06-05 08:49:35 +0800

    refactor(cinema_hall): 移除CinemaHall和CinemaHallGrom中的Capacity字段，优化数据结构

--- internal/domain/cinema/cinema_hall.go
+++ internal/domain/cinema/cinema_hall.go
@@ -10,7 +10,6 @@ type CinemaHall struct {
 	Name        string          // 影厅名称
 	ScreenType  string          // 屏幕类型
 	SoundSystem string          // 音响系统
-	Capacity    int             // 总座位数
 
 	// 多对多关系
 	Seats []*Seat // 聚合内部可以直接持有同一聚合内其他实体的引用
--- internal/infrastructure/persistence/mysql/models/cinema_hall_gorm.go
+++ internal/infrastructure/persistence/mysql/models/cinema_hall_gorm.go
@@ -12,7 +12,6 @@ type CinemaHallGrom struct {
 	Name        string `gorm:"type:varchar(50);uniqueIndex;not null;"` // 影厅名称 (例如: "1号厅", "IMAX厅")。
 	ScreenType  string `gorm:"type:varchar(50)"`                       // 屏幕类型，如 "2D", "3D", "IMAX"
 	SoundSystem string `gorm:"type:varchar(100)"`                      // 音响系统
-	Capacity    int    // 总座位数
 
 	Seats []SeatGrom `gorm:"foreignKey:CinemaHallID;OnDelete:CASCADE"`
 }
@@ -30,23 +29,16 @@ func (c *CinemaHallGrom) ToDomain() *cinema.CinemaHall {
 		Name:        c.Name,
 		ScreenType:  c.ScreenType,
 		SoundSystem: c.SoundSystem,
-		Capacity:    c.Capacity,
 		Seats:       seats,
 	}
 }
 
 // CinemaHallGromFromDomain 将CinemaHall转换为CinemaHallGorm（开销过大，需要在业务层处理）
 func CinemaHallGromFromDomain(c *cinema.CinemaHall) *CinemaHallGrom {
-	seats := make([]SeatGrom, len(c.Seats))
-	for i, seat := range c.Seats {
-		seats[i] = *SeatGromFromDomain(seat)
-	}
 	return &CinemaHallGrom{
 		Model:       gorm.Model{ID: uint(c.ID)},
 		Name:        c.Name,
 		ScreenType:  c.ScreenType,
 		SoundSystem: c.SoundSystem,
-		Capacity:    c.Capacity,
-		Seats:       seats,
 	}
 }
