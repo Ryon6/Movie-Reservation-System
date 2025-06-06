afec8d75851ae54e4f3987783308a6eefda32e64
peng <peng.nullptr@qq.com>
2025-06-06 23:14:55 +0800

    feat(auth_response): 更新LoginResponse结构体，修改User字段为指针类型，并新增ToLoginResponse函数以简化登录响应的创建

--- internal/api/dto/response/auth_response.go
+++ internal/api/dto/response/auth_response.go
@@ -1,10 +1,21 @@
 package response
 
-import "time"
+import (
+	"mrs/internal/domain/user"
+	"time"
+)
 
 // LoginResponse 定义了成功登录后返回的结构体。
 type LoginResponse struct {
-	Token     string       `json:"token"`
-	ExpiresAt time.Time    `json:"expires_at"`
-	User      UserResponse `json:"user"`
+	Token     string        `json:"token"`
+	ExpiresAt time.Time     `json:"expires_at"`
+	User      *UserResponse `json:"user"`
+}
+
+func ToLoginResponse(token string, expiresAt time.Time, user *user.User) *LoginResponse {
+	return &LoginResponse{
+		Token:     token,
+		ExpiresAt: expiresAt,
+		User:      ToUserResponse(user),
+	}
 }
