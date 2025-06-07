40d65165be3f1a415f1e3fd373cc67ab39b70556
peng <peng.nullptr@qq.com>
2025-06-07 10:02:40 +0800

    fix(seat_gorm): 修正SeatGrom为SeatGorm的拼写错误，确保一致性

--- internal/infrastructure/persistence/mysql/models/seat_gorm.go
+++ internal/infrastructure/persistence/mysql/models/seat_gorm.go
@@ -7,17 +7,17 @@ import (
 	"gorm.io/gorm"
 )
 
-type SeatGrom struct {
+type SeatGorm struct {
 	gorm.Model
 	// 对 CinemaHallID 单独建立索引: 应对高频查询影厅全部座位的需求
 	CinemaHallID  uint           `gorm:"not null;index;uniqueIndex:idx_hall_row_number"` // 联合唯一索引
-	CinemaHall    CinemaHallGrom `gorm:"foreignKey:CinemaHallID"`
+	CinemaHall    CinemaHallGorm `gorm:"foreignKey:CinemaHallID"`
 	RowIdentifier string         `gorm:"type:varchar(10);not null;uniqueIndex:idx_hall_row_number"` // 联合唯一索引
 	SeatNumber    string         `gorm:"type:varchar(10);not null;uniqueIndex:idx_hall_row_number"`
 	Type          string         `gorm:"type:varchar(50);default:'STANDARD'"`
 }
 
-func (s *SeatGrom) ToDomain() *cinema.Seat {
+func (s *SeatGorm) ToDomain() *cinema.Seat {
 	return &cinema.Seat{
 		ID:            vo.SeatID(s.ID),
 		CinemaHallID:  vo.CinemaHallID(s.CinemaHallID),
@@ -27,8 +27,8 @@ func (s *SeatGrom) ToDomain() *cinema.Seat {
 	}
 }
 
-func SeatGromFromDomain(s *cinema.Seat) *SeatGrom {
-	return &SeatGrom{
+func SeatGormFromDomain(s *cinema.Seat) *SeatGorm {
+	return &SeatGorm{
 		Model:         gorm.Model{ID: uint(s.ID)},
 		CinemaHallID:  uint(s.CinemaHallID),
 		RowIdentifier: s.RowIdentifier,
