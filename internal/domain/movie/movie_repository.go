d02d20ee63e64164082af2715bd4a34e16ea761d
peng <peng.nullptr@qq.com>
2025-06-08 07:42:19 +0800

    feat(movie_repository): 更新电影仓库接口，调整列表方法参数为MovieQueryOptions结构体，新增查询选项以支持更灵活的电影列表查询

--- internal/domain/movie/movie_repository.go
+++ internal/domain/movie/movie_repository.go
@@ -10,11 +10,19 @@ type MovieRepository interface {
 	FindByIDs(ctx context.Context, ids []uint) ([]*Movie, error)
 	FindByTitle(ctx context.Context, title string) (*Movie, error)
 	// 分页和过滤，返回总数
-	List(ctx context.Context, page, pageSize int, filters map[string]interface{}) ([]*Movie, int64, error)
+	List(ctx context.Context, options *MovieQueryOptions) ([]*Movie, int64, error)
 	Update(ctx context.Context, movie *Movie) error
 	Delete(ctx context.Context, id uint) error
 	// 为电影增加、删除和修改类型
 	AddGenreToMovie(ctx context.Context, movie *Movie, genre *Genre) error
 	RemoveGenreToMovie(ctx context.Context, movie *Movie, genre *Genre) error
 	ReplaceGenresForMovie(ctx context.Context, movie *Movie, genres []*Genre) error
+}
+
+type MovieQueryOptions struct {
+	Title       string // 标题（模糊查询）
+	ReleaseYear int    // 上映年份
+	GenreName   string // 类型
+	Page        int    // 页码（从1开始）
+	PageSize    int    // 每页数量
 }
