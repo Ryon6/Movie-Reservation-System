c4334dbb9cae010b1acc805c78c135f871a02891
peng <peng.nullptr@qq.com>
2025-06-08 07:42:40 +0800

    refactor(movie): 优化电影实体结构，调整字段顺序并添加注释，明确必选和可选字段

--- internal/domain/movie/movie.go
+++ internal/domain/movie/movie.go
@@ -5,20 +5,16 @@ import (
 	"time"
 )
 
-// 电影
+// 电影实体(必选：Title，类型，上映日期)
 type Movie struct {
-	ID              vo.MovieID
-	Title           string
-	Description     string
-	PosterURL       string
-	DurationMinutes int
-
-	// 多对多关系
-	Genres []*Genre // 聚合内部可以直接持有同一聚合内其他实体的引用
-
-	// 可选
-	ReleaseDate time.Time // 上映日期（可空）
-	Rating      float32   // 评分
-	AgeRating   string    // 年龄分级 (例如 PG-13)
-	Cast        string    // 主要演员 (简单起见用文本，复杂系统可设计为关联表)
+	ID              vo.MovieID // 电影ID
+	Title           string     // 标题
+	PosterURL       string     // 海报URL
+	DurationMinutes int        // 时长
+	ReleaseDate     time.Time  // 上映日期
+	Description     string     // 描述
+	Rating          float32    // 评分
+	AgeRating       string     // 年龄分级 (例如 PG-13)
+	Cast            string     // 主要演员 (简单起见用文本，复杂系统可设计为关联表)
+	Genres          []*Genre   // 类型（多对多关系）
 }
