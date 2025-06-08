d38fb85bf12a3866101a9d3ceb0165464b883564
peng <peng.nullptr@qq.com>
2025-06-09 06:10:38 +0800

    feat(user_request, user_response): 新增用户和角色请求及响应结构体，完善用户管理和角色管理功能

--- internal/api/dto/request/user_request.go
+++ internal/api/dto/request/user_request.go
@@ -1,13 +1,87 @@
 package request
 
+import (
+	"mrs/internal/domain/shared/vo"
+	"mrs/internal/domain/user"
+)
+
 // RegisterUserRequest 定义了用户注册请求的结构体。
 type RegisterUserRequest struct {
-	Username    string `json:"username" binding:"required,alphanum,min=3,max=50"`
-	Password    string `json:"password" binding:"required,min=8,max=100"`
-	Email       string `json:"email" binding:"required,email"`
-	DefaultRole string `json:"default_role" binding:"omitempty,alphanum"` // 可选，如果允许客户端指定
+	Username string `json:"username" binding:"required,alphanum,min=3,max=50"`
+	Password string `json:"password" binding:"required,min=8,max=100"`
+	Email    string `json:"email" binding:"required,email"`
 }
 
 type GetUserRequest struct {
-	ID uint `json:"id" binding:"required,min=1"`
+	ID uint
+}
+
+type UpdateUserRequest struct {
+	ID       uint
+	Username string `json:"username" binding:"omitempty,alphanum,min=3,max=50"`
+	Password string `json:"password" binding:"omitempty,min=8,max=100"`
+	Email    string `json:"email" binding:"omitempty,email"`
+}
+
+func (r *UpdateUserRequest) ToDomain() *user.User {
+	return &user.User{
+		ID:       vo.UserID(r.ID),
+		Username: r.Username,
+		Email:    r.Email,
+	}
+}
+
+type DeleteUserRequest struct {
+	ID uint
+}
+
+// ListUserRequest 定义了用户列表请求的结构体。
+type ListUserRequest struct {
+	PaginationRequest
+}
+
+func (r *ListUserRequest) ToDomain() *user.UserQueryOptions {
+	return &user.UserQueryOptions{
+		Page:     r.Page,
+		PageSize: r.PageSize,
+	}
+}
+
+// CreateRoleRequest 定义了创建角色请求的结构体。
+type CreateRoleRequest struct {
+	Name        string `json:"name" binding:"required,min=3,max=50"`
+	Description string `json:"description" binding:"omitempty,max=255"`
+}
+
+func (r *CreateRoleRequest) ToDomain() *user.Role {
+	return &user.Role{
+		Name:        r.Name,
+		Description: r.Description,
+	}
+}
+
+// UpdateRoleRequest 定义了更新角色请求的结构体。
+type UpdateRoleRequest struct {
+	ID          uint   `json:"id" binding:"required,min=1"`
+	Name        string `json:"name" binding:"omitempty,min=3,max=50"`
+	Description string `json:"description" binding:"omitempty,max=255"`
+}
+
+func (r *UpdateRoleRequest) ToDomain() *user.Role {
+	return &user.Role{
+		ID:          vo.RoleID(r.ID),
+		Name:        r.Name,
+		Description: r.Description,
+	}
+}
+
+// DeleteRoleRequest 定义了删除角色请求的结构体。
+type DeleteRoleRequest struct {
+	ID uint
+}
+
+// 为用户分配角色
+type AssignRoleToUserRequest struct {
+	UserID uint
+	RoleID uint
 }
--- internal/api/dto/response/user_response.go
+++ internal/api/dto/response/user_response.go
@@ -2,8 +2,8 @@ package response
 
 import "mrs/internal/domain/user"
 
-type UserResponse struct {
-	ID       uint   `json:"id"`
+type UserProfileResponse struct {
+	// ID       uint   `json:"id"`
 	Username string `json:"username"`
 	Email    string `json:"email"`
 	RoleName string `json:"role_name"` // 来自关联的 Role 实体的 Name 字段
@@ -12,11 +12,72 @@ type UserResponse struct {
 	// IsActive bool      `json:"is_active"`
 }
 
+func ToUserProfileResponse(user *user.User) *UserProfileResponse {
+	return &UserProfileResponse{
+		// ID:       uint(user.ID),
+		Username: user.Username,
+		Email:    user.Email,
+		RoleName: user.Role.Name,
+	}
+}
+
+type UserResponse struct {
+	ID       uint   `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	RoleName string `json:"role_name"`
+	RoleID   uint   `json:"role_id"`
+}
+
 func ToUserResponse(user *user.User) *UserResponse {
 	return &UserResponse{
 		ID:       uint(user.ID),
 		Username: user.Username,
 		Email:    user.Email,
 		RoleName: user.Role.Name,
+		RoleID:   uint(user.Role.ID),
 	}
+}
+
+type ListUserResponse struct {
+	PaginationResponse
+	Users []*UserResponse `json:"users"`
+}
+
+func ToListUserResponse(users []*user.User) *ListUserResponse {
+	userResponses := make([]*UserResponse, 0, len(users))
+	for _, user := range users {
+		userResponses = append(userResponses, ToUserResponse(user))
+	}
+	return &ListUserResponse{
+		Users: userResponses,
+	}
+}
+
+// RoleResponse 定义了角色响应的结构体。
+type RoleResponse struct {
+	ID          uint   `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
+
+func ToRoleResponse(role *user.Role) *RoleResponse {
+	return &RoleResponse{
+		ID:          uint(role.ID),
+		Name:        role.Name,
+		Description: role.Description,
+	}
+}
+
+// ListRoleResponse 定义了角色列表响应的结构体。
+type ListRoleResponse struct {
+	Roles []*RoleResponse `json:"roles"`
+}
+
+func ToListRoleResponse(roles []*user.Role) *ListRoleResponse {
+	roleResponses := make([]*RoleResponse, 0, len(roles))
+	for _, role := range roles {
+		roleResponses = append(roleResponses, ToRoleResponse(role))
+	}
+	return &ListRoleResponse{Roles: roleResponses}
 }
