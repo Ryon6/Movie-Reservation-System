63c6b8e79a58668b765d3c8670088304c8c122db
peng <peng.nullptr@qq.com>
2025-06-10 11:44:42 +0800

    feat(cinema_request): 新增影厅更新和删除请求结构体，优化影厅创建时座位处理逻辑

--- internal/api/dto/request/cinema_request.go
+++ internal/api/dto/request/cinema_request.go
@@ -7,16 +7,25 @@ import (
 
 // 创建影厅
 type CreateCinemaHallRequest struct {
-	Name        string `json:"name" binding:"required,min=1,max=255"`
-	ScreenType  string `json:"screen_type" binding:"required,min=1,max=255"`
-	SoundSystem string `json:"sound_system" binding:"required,min=1,max=255"`
+	Name        string         `json:"name" binding:"required,min=1,max=255"`
+	ScreenType  string         `json:"screen_type" binding:"required,min=1,max=255"`
+	SoundSystem string         `json:"sound_system" binding:"required,min=1,max=255"`
+	Seats       []*SeatRequest `json:"seats" binding:"omitempty"` // 影厅座位，如果为空，则自动生成默认座位布局
 }
 
 func (r *CreateCinemaHallRequest) ToDomain() *cinema.CinemaHall {
+	seats := make([]*cinema.Seat, 0)
+	if len(r.Seats) != 0 {
+		seats = make([]*cinema.Seat, len(r.Seats))
+		for i, seat := range r.Seats {
+			seats[i] = seat.ToDomain()
+		}
+	}
 	return &cinema.CinemaHall{
 		Name:        r.Name,
 		ScreenType:  r.ScreenType,
 		SoundSystem: r.SoundSystem,
+		Seats:       seats,
 	}
 }
 
@@ -27,7 +36,7 @@ type GetCinemaHallRequest struct {
 type SeatRequest struct {
 	RowIdentifier string `json:"row_identifier" binding:"required,min=1,max=255"`
 	SeatNumber    string `json:"seat_number" binding:"required,min=1,max=255"`
-	Type          string `json:"type" binding:"required,min=1,max=255"`
+	Type          string `json:"type" binding:"omitempty,min=1,max=255"`
 }
 
 func (r *SeatRequest) ToDomain() *cinema.Seat {
@@ -40,7 +49,7 @@ func (r *SeatRequest) ToDomain() *cinema.Seat {
 
 type CreateHallSeatsRequest struct {
 	CinemaHallID uint           `json:"cinema_hall_id" binding:"required,min=1"`
-	Seats        []*SeatRequest `json:"seats" binding:"required,min=1"`
+	Seats        []*SeatRequest `json:"seats" binding:"required"`
 }
 
 func (r *CreateHallSeatsRequest) ToDomain() []*cinema.Seat {
@@ -50,4 +59,24 @@ func (r *CreateHallSeatsRequest) ToDomain() []*cinema.Seat {
 		domainSeats[i].CinemaHallID = vo.CinemaHallID(r.CinemaHallID)
 	}
 	return domainSeats
+}
+
+type UpdateCinemaHallRequest struct {
+	ID          uint
+	Name        string `json:"name" binding:"omitempty,min=1,max=255"`
+	ScreenType  string `json:"screen_type" binding:"omitempty,min=1,max=255"`
+	SoundSystem string `json:"sound_system" binding:"omitempty,min=1,max=255"`
+}
+
+func (r *UpdateCinemaHallRequest) ToDomain() *cinema.CinemaHall {
+	return &cinema.CinemaHall{
+		ID:          vo.CinemaHallID(r.ID),
+		Name:        r.Name,
+		ScreenType:  r.ScreenType,
+		SoundSystem: r.SoundSystem,
+	}
+}
+
+type DeleteCinemaHallRequest struct {
+	ID uint
 }
