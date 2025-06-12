81dbae775a70fc806d0b091bff542190603e8a68
peng <peng.nullptr@qq.com>
2025-06-12 20:15:32 +0800

    feat(showtime): 实现获取座位表功能，增加座位缓存和分布式锁支持

--- internal/app/showtime_service.go
+++ internal/app/showtime_service.go
@@ -7,9 +7,13 @@ import (
 	"math"
 	"mrs/internal/api/dto/request"
 	"mrs/internal/api/dto/response"
+	"mrs/internal/domain/cinema"
 	"mrs/internal/domain/shared"
+	"mrs/internal/domain/shared/lock"
+	"mrs/internal/domain/shared/vo"
 	"mrs/internal/domain/showtime"
 	applog "mrs/pkg/log"
+	"time"
 )
 
 type ShowtimeService interface {
@@ -18,27 +22,36 @@ type ShowtimeService interface {
 	UpdateShowtime(ctx context.Context, req *request.UpdateShowtimeRequest) (*response.ShowtimeResponse, error)
 	DeleteShowtime(ctx context.Context, req *request.DeleteShowtimeRequest) error
 	ListShowtimes(ctx context.Context, req *request.ListShowtimesRequest) (*response.PaginatedShowtimeResponse, error)
-	// GetSeatMap(ctx context.Context, req *request.GetSeatMapRequest) (*response.SeatMapResponse, error)
+	GetSeatMap(ctx context.Context, req *request.GetSeatMapRequest) (*response.SeatMapResponse, error)
 }
 
 type showtimeService struct {
-	uow       shared.UnitOfWork
-	showRepo  showtime.ShowtimeRepository
-	showCache showtime.ShowtimeCache
-	logger    applog.Logger
+	uow          shared.UnitOfWork
+	showRepo     showtime.ShowtimeRepository
+	seatRepo     cinema.SeatRepository
+	showCache    showtime.ShowtimeCache
+	seatCache    cinema.SeatCache
+	lockProvider lock.LockProvider
+	logger       applog.Logger
 }
 
 func NewShowtimeService(
 	uow shared.UnitOfWork,
 	showRepo showtime.ShowtimeRepository,
+	seatRepo cinema.SeatRepository,
 	showCache showtime.ShowtimeCache,
+	seatCache cinema.SeatCache,
+	lockProvider lock.LockProvider,
 	logger applog.Logger,
 ) ShowtimeService {
 	return &showtimeService{
-		uow:       uow,
-		showRepo:  showRepo,
-		showCache: showCache,
-		logger:    logger.With(applog.String("Service", "ShowtimeService")),
+		uow:          uow,
+		showRepo:     showRepo,
+		seatRepo:     seatRepo,
+		showCache:    showCache,
+		seatCache:    seatCache,
+		lockProvider: lockProvider,
+		logger:       logger.With(applog.String("Service", "ShowtimeService")),
 	}
 }
 
@@ -230,11 +243,80 @@ func (s *showtimeService) ListShowtimes(ctx context.Context,
 }
 
 // 获取座位表
-// func (s *showtimeService) GetSeatMap(ctx context.Context, req *request.GetSeatMapRequest) (*response.SeatMapResponse, error) {
-// 	logger := s.logger.With(applog.String("Method", "GetSeatMap"), applog.Uint("showtime_id", req.ShowtimeID))
-// 	seatMap, err := s.showCache.GetSeatMap(ctx, req.ShowtimeID)
-// 	if err != nil {
-// 		logger.Error("failed to get seat map", applog.Error(err))
-// 		return nil, err
-// 	}
-// }
+func (s *showtimeService) GetSeatMap(ctx context.Context, req *request.GetSeatMapRequest) (*response.SeatMapResponse, error) {
+	logger := s.logger.With(applog.String("Method", "GetSeatMap"), applog.Uint("showtime_id", req.ShowtimeID))
+	seatInfos, err := s.seatCache.GetSeatMap(ctx, vo.ShowtimeID(req.ShowtimeID))
+	if err == nil {
+		return &response.SeatMapResponse{Seats: seatInfos}, nil
+	}
+
+	// 缓存未命中，且不是缓存缺失错误
+	if !errors.Is(err, shared.ErrCacheMissing) {
+		logger.Error("failed to get seat map from cache", applog.Error(err))
+	}
+
+	// 获取分布式锁，防止并发初始化座位表
+	lockKey := cinema.GetShowtimeSeatsInitLockKey(vo.ShowtimeID(req.ShowtimeID))
+	// 获取锁失败，则重试
+	var initLock lock.Lock = nil
+	for i := 0; i < lock.DefaultMaxRetries; i++ {
+		initLock, err = s.lockProvider.Acquire(ctx, lockKey, lock.DefaultLockTTL)
+		if errors.Is(err, lock.ErrLockNotAcquired) {
+			logger.Warn("other process is initializing seat map, retrying...")
+			time.Sleep(lock.DefaultBackoff)
+			continue
+		}
+		if err != nil {
+			logger.Error("failed to acquire lock", applog.Error(err))
+			return nil, err
+		}
+		defer initLock.Release(ctx)
+		break
+	}
+	// 若三次获取锁失败，则返回错误
+	if initLock == nil {
+		logger.Error("retry to acquire lock failed", applog.Int("retries", lock.DefaultMaxRetries))
+		return nil, fmt.Errorf("retry to acquire lock failed")
+	}
+
+	// 获取场次信息（应用层服务，会先从缓存中获取，如果缓存未命中，则从数据库中获取）
+	showtimeResp, err := s.GetShowtime(ctx, &request.GetShowtimeRequest{ID: req.ShowtimeID})
+	if err != nil {
+		if errors.Is(err, showtime.ErrShowtimeNotFound) {
+			logger.Warn("showtime not found")
+			return nil, err
+		}
+		logger.Error("failed to get showtime", applog.Error(err))
+		return nil, err
+	}
+
+	// 获取座位表
+	seats, err := s.seatRepo.FindByHallID(ctx, uint(showtimeResp.CinemaHall.ID))
+	if err != nil {
+		logger.Error("failed to find seats", applog.Error(err))
+		return nil, err
+	}
+
+	// TODO: 获取已售座位 需要booking_repository支持
+
+	// 座位表缓存过期时间设置为场次结束时间后10分钟
+	expireTime := time.Until(showtimeResp.EndTime.Add(time.Minute * 10))
+	if err := s.seatCache.InitSeatMap(ctx, vo.ShowtimeID(req.ShowtimeID), seats, nil, expireTime); err != nil {
+		logger.Error("failed to init seat map", applog.Error(err))
+		return nil, err
+	}
+
+	// 将座位表转换为响应格式
+	seatInfos = make([]*cinema.SeatInfo, 0, len(seats))
+	for _, seat := range seats {
+		seatInfos = append(seatInfos, &cinema.SeatInfo{
+			ID:            seat.ID,
+			Type:          seat.Type,
+			RowIdentifier: seat.RowIdentifier,
+			SeatNumber:    seat.SeatNumber,
+			Status:        cinema.SeatStatusAvailable, // 先默认可用，后续再根据已售座位更新状态
+		})
+	}
+
+	return &response.SeatMapResponse{Seats: seatInfos}, nil
+}
