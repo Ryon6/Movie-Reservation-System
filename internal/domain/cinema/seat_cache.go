e68f2c32df2d5a310d647762b4b3386995c5d82b
peng <peng.nullptr@qq.com>
2025-06-12 18:11:11 +0800

    feat(cinema): 更新座位缓存接口，新增座位初始化和状态位图缓存键生成方法

--- internal/domain/cinema/seat_cache.go
+++ internal/domain/cinema/seat_cache.go
@@ -2,16 +2,45 @@ package cinema
 
 import (
 	"context"
+	"fmt"
 	"mrs/internal/domain/shared/vo"
+	"time"
 )
 
 // 关于座位缓存：将动态信息座位状态存储为位图，静态信息存储为哈希表
 
 // 座位缓存接口
 type SeatCache interface {
-	LockSeats(ctx context.Context, showtimeID vo.ShowtimeID, seatIDs []vo.SeatID) error
-	ReleaseSeats(ctx context.Context, showtimeID vo.ShowtimeID, seatIDs []vo.SeatID) error
-	GetSeatMap(ctx context.Context, showtimeID vo.ShowtimeID) ([]SeatWithStatus, error)
-	SetSeatMap(ctx context.Context, showtimeID vo.ShowtimeID, seats []SeatWithStatus) error
+	// LockSeats(ctx context.Context, showtimeID vo.ShowtimeID, seatIDs []vo.SeatID) error
+	// ReleaseSeats(ctx context.Context, showtimeID vo.ShowtimeID, seatIDs []vo.SeatID) error
+	GetSeatMap(ctx context.Context, showtimeID vo.ShowtimeID) ([]SeatInfo, error)
+	InitSeatMap(ctx context.Context, showtimeID vo.ShowtimeID, hallLayout []Seat, bookedSeatIDs []vo.SeatID, expireTime time.Duration) error
 	// DeleteSeatMap(ctx context.Context, showtimeID vo.ShowtimeID) error // 自动过期
+}
+
+const (
+	ShowtimeSeatsBitmapKeyFormat   = "showtime:%d:seats:bitmap"    // 场次座位状态位图
+	ShowtimeSeatsInfoKeyFormat     = "showtime:%d:seats:info"      // 场次座位静态信息
+	ShowtimeSeatsLockKeyFormat     = "showtime:%d:seats:locks"     // 座位临时锁定
+	ShowtimeSeatsInitLockKeyFormat = "showtime:%d:seats:init:lock" // 初始化座位表的锁，防止并发初始化座位表
+)
+
+// 生成座位临时锁定的缓存键
+func GetShowtimeSeatsLockKey(showtimeID vo.ShowtimeID) string {
+	return fmt.Sprintf(ShowtimeSeatsLockKeyFormat, showtimeID)
+}
+
+// 生成座位初始化锁定的缓存键
+func GetShowtimeSeatsInitLockKey(showtimeID vo.ShowtimeID) string {
+	return fmt.Sprintf(ShowtimeSeatsInitLockKeyFormat, showtimeID)
+}
+
+// 生成座位状态位图的缓存键
+func GetShowtimeSeatsBitmapKey(showtimeID vo.ShowtimeID) string {
+	return fmt.Sprintf(ShowtimeSeatsBitmapKeyFormat, showtimeID)
+}
+
+// 生成座位静态信息的缓存键
+func GetShowtimeSeatsInfoKey(showtimeID vo.ShowtimeID) string {
+	return fmt.Sprintf(ShowtimeSeatsInfoKeyFormat, showtimeID)
 }
