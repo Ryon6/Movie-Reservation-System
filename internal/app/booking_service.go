38757a93e744283ea6873b09c4ab276e800a4857
peng <peng.nullptr@qq.com>
2025-06-14 06:31:34 +0800

    feat(booking): 在BookingService中新增ListBookings、GetBooking、CancelBooking和ConfirmBooking方法，以支持订单管理功能

--- internal/app/booking_service.go
+++ internal/app/booking_service.go
@@ -16,16 +16,21 @@ import (
 
 type BookingService interface {
 	CreateBooking(ctx context.Context, req *request.CreateBookingRequest) (*response.BookingResponse, error)
+	ListBookings(ctx context.Context, req *request.ListBookingsRequest) (*response.ListBookingsResponse, error)
+	GetBooking(ctx context.Context, req *request.GetBookingRequest) (*response.BookingResponse, error)
+	CancelBooking(ctx context.Context, req *request.CancelBookingRequest) (*response.BookingResponse, error)
+	ConfirmBooking(ctx context.Context, req *request.ConfirmBookingRequest) (*response.BookingResponse, error)
 }
 
 type bookingService struct {
-	uow           shared.UnitOfWork
-	bookingRepo   booking.BookingRepository
-	showtimeRepo  showtime.ShowtimeRepository
-	seatCache     cinema.SeatCache
-	showtimeCache showtime.ShowtimeCache
-	lockProvider  lock.LockProvider
-	logger        applog.Logger
+	uow             shared.UnitOfWork
+	bookingRepo     booking.BookingRepository
+	showtimeRepo    showtime.ShowtimeRepository
+	seatCache       cinema.SeatCache
+	showtimeCache   showtime.ShowtimeCache
+	showtimeService ShowtimeService
+	lockProvider    lock.LockProvider
+	logger          applog.Logger
 }
 
 func NewBookingService(
@@ -34,17 +39,19 @@ func NewBookingService(
 	showtimeRepo showtime.ShowtimeRepository,
 	seatCache cinema.SeatCache,
 	showtimeCache showtime.ShowtimeCache,
+	showtimeService ShowtimeService,
 	lockProvider lock.LockProvider,
 	logger applog.Logger) BookingService {
 
 	return &bookingService{
-		uow:           uow,
-		bookingRepo:   bookingRepo,
-		showtimeRepo:  showtimeRepo,
-		seatCache:     seatCache,
-		showtimeCache: showtimeCache,
-		lockProvider:  lockProvider,
-		logger:        logger.With(applog.String("Service", "BookingService")),
+		uow:             uow,
+		bookingRepo:     bookingRepo,
+		showtimeRepo:    showtimeRepo,
+		seatCache:       seatCache,
+		showtimeCache:   showtimeCache,
+		showtimeService: showtimeService,
+		lockProvider:    lockProvider,
+		logger:          logger.With(applog.String("Service", "BookingService")),
 	}
 }
 
@@ -54,18 +61,12 @@ func (s *bookingService) CreateBooking(ctx context.Context, req *request.CreateB
 	lockKey := cinema.GetShowtimeSeatsLockKey(vo.ShowtimeID(req.ShowtimeID))
 
 	// 获取场次信息
-	st, err := s.showtimeCache.GetShowtime(ctx, vo.ShowtimeID(req.ShowtimeID))
+	st, err := s.showtimeService.GetShowtime(ctx, &request.GetShowtimeRequest{
+		ID: req.ShowtimeID,
+	})
 	if err != nil {
-		logger.Error("failed to get showtime from cache", applog.Error(err))
-	} else if st == nil {
-		st, err = s.showtimeRepo.FindByID(ctx, vo.ShowtimeID(req.ShowtimeID))
-		if err != nil {
-			logger.Error("failed to get showtime from repo", applog.Error(err))
-			return nil, err
-		}
-		if err := s.showtimeCache.SetShowtime(ctx, st, showtime.DefaultExpiration); err != nil {
-			logger.Error("failed to set showtime to cache", applog.Error(err))
-		}
+		logger.Error("failed to get showtime by service", applog.Error(err))
+		return nil, err
 	}
 
 	// 获取分布式锁（场次锁）
@@ -113,7 +114,7 @@ func (s *bookingService) CreateBooking(ctx context.Context, req *request.CreateB
 	err = s.uow.Execute(ctx, func(ctx context.Context, provider shared.RepositoryProvider) error {
 		bookingRepo := provider.GetBookingRepository()
 		bookedSeatRepo := provider.GetBookedSeatRepository()
-		booking, err = bookingRepo.CreateBooking(ctx, booking)
+		booking, err = bookingRepo.Create(ctx, booking)
 		if err != nil {
 			logger.Error("failed to create booking", applog.Error(err))
 			return err
@@ -125,7 +126,7 @@ func (s *bookingService) CreateBooking(ctx context.Context, req *request.CreateB
 		}
 
 		// 创建已预订的座位
-		bookedSeats, err = bookedSeatRepo.CreateBookedSeats(ctx, bookedSeats)
+		bookedSeats, err = bookedSeatRepo.CreateBatch(ctx, bookedSeats)
 		if err != nil {
 			logger.Error("failed to create booked seats", applog.Error(err))
 			return err
@@ -140,4 +141,121 @@ func (s *bookingService) CreateBooking(ctx context.Context, req *request.CreateB
 
 	logger.Info("create booking successfully", applog.Float64("total_price", totalPrice))
 	return response.ToBookingResponse(booking), nil
+}
+
+// ListBookings 查询订单列表
+func (s *bookingService) ListBookings(ctx context.Context, req *request.ListBookingsRequest) (*response.ListBookingsResponse, error) {
+	logger := s.logger.With(applog.String("Method", "ListBookings"))
+
+	domainOptions := req.ToDomain()
+	bookings, totalCount, err := s.bookingRepo.List(ctx, domainOptions)
+	if err != nil {
+		logger.Error("failed to list bookings", applog.Error(err))
+		return nil, err
+	}
+	response := response.ToListBookingsResponse(bookings, int(totalCount), &req.PaginationRequest)
+	return response, nil
+}
+
+// GetBooking 查询订单
+func (s *bookingService) GetBooking(ctx context.Context, req *request.GetBookingRequest) (*response.BookingResponse, error) {
+	logger := s.logger.With(applog.String("Method", "GetBooking"))
+
+	booking, err := s.bookingRepo.FindByID(ctx, vo.BookingID(req.ID))
+	if err != nil {
+		logger.Error("failed to get booking", applog.Error(err))
+		return nil, err
+	}
+	return response.ToBookingResponse(booking), nil
+}
+
+// CancelBooking 取消订单
+func (s *bookingService) CancelBooking(ctx context.Context, req *request.CancelBookingRequest) (*response.BookingResponse, error) {
+	logger := s.logger.With(applog.String("Method", "CancelBooking"))
+
+	lockKey := cinema.GetShowtimeSeatsLockKey(vo.ShowtimeID(req.ID))
+	lock, err := s.lockProvider.Acquire(ctx, lockKey, lock.DefaultLockTTL)
+	if err != nil {
+		logger.Error("failed to acquire lock", applog.Error(err))
+		return nil, err
+	}
+	defer lock.Release(ctx)
+
+	var bk *booking.Booking
+	// 使用事务，保证操作的原子性
+	err = s.uow.Execute(ctx, func(ctx context.Context, provider shared.RepositoryProvider) error {
+		bookingRepo := provider.GetBookingRepository()
+		bk, err = bookingRepo.FindByID(ctx, vo.BookingID(req.ID))
+		if err != nil {
+			logger.Error("failed to get booking", applog.Error(err))
+			return err
+		}
+
+		// 订单状态必须是pending
+		if bk.Status != booking.BookingStatusPending {
+			logger.Warn("booking is not pending", applog.String("status", string(bk.Status)))
+			return booking.ErrBookingNotPending
+		}
+
+		// 底层已预加载座位信息
+		seatIDs := make([]vo.SeatID, len(bk.BookedSeats))
+		for i, bookedSeat := range bk.BookedSeats {
+			seatIDs[i] = bookedSeat.SeatID
+		}
+
+		// 释放座位锁
+		if err = s.seatCache.ReleaseSeats(ctx, vo.ShowtimeID(req.ID), seatIDs); err != nil {
+			logger.Error("failed to release seats", applog.Error(err))
+			return err
+		}
+
+		bk.Cancel()
+		if err = bookingRepo.Update(ctx, bk); err != nil {
+			logger.Error("failed to update booking", applog.Error(err))
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		logger.Error("failed to cancel booking", applog.Error(err))
+		return nil, err
+	}
+
+	logger.Info("cancel booking successfully", applog.String("status", string(bk.Status)))
+	return response.ToBookingResponse(bk), nil
+}
+
+// ConfirmBooking 确认订单（简单实现，后续需要对接支付系统）
+func (s *bookingService) ConfirmBooking(ctx context.Context, req *request.ConfirmBookingRequest) (*response.BookingResponse, error) {
+	logger := s.logger.With(applog.String("Method", "ConfirmBooking"))
+
+	var bk *booking.Booking
+	var err error
+	err = s.uow.Execute(ctx, func(ctx context.Context, provider shared.RepositoryProvider) error {
+		bookingRepo := provider.GetBookingRepository()
+		bk, err = bookingRepo.FindByID(ctx, vo.BookingID(req.ID))
+		if err != nil {
+			logger.Error("failed to get booking", applog.Error(err))
+			return err
+		}
+
+		if bk.Status != booking.BookingStatusPending {
+			logger.Warn("booking is not pending", applog.String("status", string(bk.Status)))
+			return booking.ErrBookingNotPending
+		}
+
+		bk.Confirm()
+		if err = bookingRepo.Update(ctx, bk); err != nil {
+			logger.Error("failed to update booking", applog.Error(err))
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		logger.Error("failed to confirm booking", applog.Error(err))
+		return nil, err
+	}
+
+	logger.Info("confirm booking successfully", applog.String("status", string(bk.Status)))
+	return response.ToBookingResponse(bk), nil
 }
