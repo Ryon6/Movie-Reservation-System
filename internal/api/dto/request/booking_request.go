ad3994c548e4e705f0523c4f7857cf9bf353ddbc
peng <peng.nullptr@qq.com>
2025-06-14 04:32:14 +0800

    feat(booking): 新增ListBookingsRequest结构体及其ToDomain方法，以支持查询订单功能

--- internal/api/dto/request/booking_request.go
+++ internal/api/dto/request/booking_request.go
@@ -1,8 +1,29 @@
 package request
 
+import (
+	"mrs/internal/domain/booking"
+	"mrs/internal/domain/shared/vo"
+)
+
 // 创建订单请求
 type CreateBookingRequest struct {
 	UserID     uint
 	ShowtimeID uint   `json:"showtime_id"`
 	SeatIDs    []uint `json:"seat_ids"`
+}
+
+// 查询订单请求
+type ListBookingsRequest struct {
+	PaginationRequest
+	UserID uint
+	Status string `json:"status" binding:"omitempty,min=1,max=255"`
+}
+
+func (r *ListBookingsRequest) ToDomain() *booking.BookingQueryOptions {
+	return &booking.BookingQueryOptions{
+		UserID:   vo.UserID(r.UserID),
+		Status:   booking.BookingStatus(r.Status),
+		Page:     r.Page,
+		PageSize: r.PageSize,
+	}
 }
