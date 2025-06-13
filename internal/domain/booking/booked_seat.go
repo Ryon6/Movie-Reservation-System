ad4277c3786d9dbcef2276faf1027df47ac6925b
peng <peng.nullptr@qq.com>
2025-06-13 15:06:54 +0800

    fix(booking): 在BookedSeat结构体中新增ShowtimeID字段，以关联放映场次ID

--- internal/domain/booking/booked_seat.go
+++ internal/domain/booking/booked_seat.go
@@ -6,10 +6,11 @@ import "mrs/internal/domain/shared/vo"
 
 // BookedSeat 表示一个已预订的座位
 type BookedSeat struct {
-	ID        vo.BookedSeatID // 主键
-	BookingID vo.BookingID    // 关联的预订ID
-	SeatID    vo.SeatID       // 关联的座位ID
-	Price     float64         // 座位价格
+	ID         vo.BookedSeatID // 主键
+	BookingID  vo.BookingID    // 关联的预订ID
+	ShowtimeID vo.ShowtimeID   // 关联的放映场次ID
+	SeatID     vo.SeatID       // 关联的座位ID
+	Price      float64         // 座位价格
 }
 
 // 约束：对于(ShowtimeID, SeatID)组合应有唯一约束
