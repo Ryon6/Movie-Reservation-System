2ca7102fffaa86b0f510b405a92fb8d39eb0f305
peng <peng.nullptr@qq.com>
2025-06-14 06:29:22 +0800

    feat(booking): 新增ListBookingsResponse结构体及其转换方法，以支持订单列表的API响应

--- internal/api/dto/response/booking_response.go
+++ internal/api/dto/response/booking_response.go
@@ -1,6 +1,8 @@
 package response
 
 import (
+	"math"
+	"mrs/internal/api/dto/request"
 	"mrs/internal/domain/booking"
 	"time"
 )
@@ -19,5 +21,28 @@ func ToBookingResponse(booking *booking.Booking) *BookingResponse {
 		TotalAmount: booking.TotalAmount,
 		BookingTime: booking.BookingTime,
 		Status:      string(booking.Status),
+	}
+}
+
+// ListBookingsResponse 表示一个订单列表的响应
+type ListBookingsResponse struct {
+	Bookings []*BookingResponse `json:"bookings"`
+	PaginationResponse
+}
+
+// ToListBookingsResponse 将domain层的订单列表转换为API层的订单列表
+func ToListBookingsResponse(bookings []*booking.Booking, totalCount int, req *request.PaginationRequest) *ListBookingsResponse {
+	response := make([]*BookingResponse, len(bookings))
+	for i, booking := range bookings {
+		response[i] = ToBookingResponse(booking)
+	}
+	return &ListBookingsResponse{
+		Bookings: response,
+		PaginationResponse: PaginationResponse{
+			Page:       req.Page,
+			PageSize:   req.PageSize,
+			TotalCount: totalCount,
+			TotalPages: int(math.Ceil(float64(totalCount) / float64(req.PageSize))),
+		},
 	}
 }
