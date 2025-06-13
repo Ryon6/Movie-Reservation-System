b8ed347eb1ac97f63d6dfff2193a47eec452ad8d
peng <peng.nullptr@qq.com>
2025-06-13 15:07:38 +0800

    feat(booking): 在BookedSeatGorm结构体中添加ShowtimeID字段，并更新ToDomain和BookedSeatGormFromDomain方法以支持放映场次ID

--- internal/infrastructure/persistence/mysql/models/booked_seat_gorm.go
+++ internal/infrastructure/persistence/mysql/models/booked_seat_gorm.go
@@ -9,11 +9,14 @@ import (
 
 type BookedSeatGorm struct {
 	gorm.Model
-	BookingID uint        `gorm:"not null;index;foreignKey:BookingID,references:ID"`
-	SeatID    uint        `gorm:"not null;index;foreignKey:SeatID,references:ID"`
-	Price     float64     `gorm:"not null"`
-	Booking   BookingGorm `gorm:"foreignKey:BookingID"`
-	Seat      SeatGorm    `gorm:"foreignKey:SeatID"`
+	BookingID uint `gorm:"not null;index;foreignKey:BookingID,references:ID"`
+	// 联合唯一索引(唯一约束保证每个座位只能被预订一次)
+	ShowtimeID uint         `gorm:"not null;uniqueIndex:idx_showtime_seat;foreignKey:ShowtimeID,references:ID"`
+	SeatID     uint         `gorm:"not null;uniqueIndex:idx_showtime_seat;foreignKey:SeatID,references:ID"`
+	Price      float64      `gorm:"not null"`
+	Booking    BookingGorm  `gorm:"foreignKey:BookingID"`
+	Showtime   ShowtimeGorm `gorm:"foreignKey:ShowtimeID"`
+	Seat       SeatGorm     `gorm:"foreignKey:SeatID"`
 }
 
 func (BookedSeatGorm) TableName() string {
@@ -22,18 +25,20 @@ func (BookedSeatGorm) TableName() string {
 
 func (b *BookedSeatGorm) ToDomain() *booking.BookedSeat {
 	return &booking.BookedSeat{
-		ID:        vo.BookedSeatID(b.ID),
-		BookingID: vo.BookingID(b.BookingID),
-		SeatID:    vo.SeatID(b.SeatID),
-		Price:     b.Price,
+		ID:         vo.BookedSeatID(b.ID),
+		BookingID:  vo.BookingID(b.BookingID),
+		ShowtimeID: vo.ShowtimeID(b.ShowtimeID),
+		SeatID:     vo.SeatID(b.SeatID),
+		Price:      b.Price,
 	}
 }
 
 func BookedSeatGormFromDomain(b *booking.BookedSeat) *BookedSeatGorm {
 	return &BookedSeatGorm{
-		Model:     gorm.Model{ID: uint(b.ID)},
-		BookingID: uint(b.BookingID),
-		SeatID:    uint(b.SeatID),
-		Price:     b.Price,
+		Model:      gorm.Model{ID: uint(b.ID)},
+		BookingID:  uint(b.BookingID),
+		ShowtimeID: uint(b.ShowtimeID),
+		SeatID:     uint(b.SeatID),
+		Price:      b.Price,
 	}
 }
