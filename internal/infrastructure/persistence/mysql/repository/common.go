0caf90d1a03426d7354098354d8fce4bf4541b1f
peng <peng.nullptr@qq.com>
2025-06-18 19:37:43 +0800

    refactor(repository): 删除了MySQL外键约束错误检查的实现代码，暂时注释掉相关函数以便后续重构。

--- internal/infrastructure/persistence/mysql/repository/common.go
+++ internal/infrastructure/persistence/mysql/repository/common.go
@@ -1,16 +1,10 @@
 package repository
 
-import (
-	"errors"
-
-	"github.com/go-sql-driver/mysql"
-)
-
 // isForeignKeyConstraintError 检查错误是否是MySQL外键约束错误（错误码1451）
-func isForeignKeyConstraintError(err error) bool {
-	var mysqlErr *mysql.MySQLError
-	if errors.As(err, &mysqlErr) {
-		return mysqlErr.Number == 1451
-	}
-	return false
-}
+// func isForeignKeyConstraintError(err error) bool {
+// 	var mysqlErr *mysql.MySQLError
+// 	if errors.As(err, &mysqlErr) {
+// 		return mysqlErr.Number == 1451
+// 	}
+// 	return false
+// }
