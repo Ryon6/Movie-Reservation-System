f79e66e0fee25898c625d884c460aa5c80c6d4c4
peng <peng.nullptr@qq.com>
2025-06-18 19:59:27 +0800

    refactor(user_gorm): 优化UserGorm模型，移除不必要的时间导入，修复UserGormFromDomain函数逻辑错误，使其能正确转换外键role_id

--- internal/infrastructure/persistence/mysql/models/user_gorm.go
+++ internal/infrastructure/persistence/mysql/models/user_gorm.go
@@ -3,7 +3,6 @@ package models
 import (
 	"mrs/internal/domain/shared/vo"
 	"mrs/internal/domain/user"
-	"time"
 
 	"gorm.io/gorm"
 )
@@ -11,11 +10,9 @@ import (
 // 用户表
 type UserGorm struct {
 	gorm.Model
-	Username     string     `gorm:"varchar(100),uniqueIndex,not null"`
-	PasswordHash string     `gorm:"varchar(255),not null"`             // 存储密码的哈希值
-	Email        string     `gorm:"varchar(255),uniqueIndex,not null"` // 用户邮箱，唯一索引
-	FullName     string     `gorm:"varchar(100)"`                      // 用户全名（可选）
-	LastLogin    *time.Time // 最后登录时间（可选）,使用指针可为null
+	Username     string `gorm:"varchar(100),uniqueIndex,not null"`
+	PasswordHash string `gorm:"varchar(255),not null"`             // 存储密码的哈希值
+	Email        string `gorm:"varchar(255),uniqueIndex,not null"` // 用户邮箱，唯一索引
 
 	RoleID uint     `gorm:"not null"`           // 关联的角色ID
 	Role   RoleGorm `gorm:"foreignKey:RoleID "` // 通常会隐式推断，这里显式定义防止出错
@@ -37,11 +34,15 @@ func (u *UserGorm) ToDomain() *user.User {
 }
 
 func UserGormFromDomain(u *user.User) *UserGorm {
-	return &UserGorm{
+	usr := &UserGorm{
 		Model:        gorm.Model{ID: uint(u.ID)},
 		Username:     u.Username,
 		Email:        u.Email,
 		PasswordHash: u.PasswordHash,
-		Role:         *RoleGormFromDomain(u.Role),
 	}
+	if u.Role != nil {
+		usr.Role = *RoleGormFromDomain(u.Role)
+		usr.RoleID = uint(u.Role.ID)
+	}
+	return usr
 }
