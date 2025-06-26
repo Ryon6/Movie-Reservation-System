dca781124844bb06106699e254179fa7d794dca9
peng <peng.nullptr@qq.com>
2025-06-26 18:03:12 +0800

    feat(cache): 修改影厅缓存接口，更新获取所有影厅的方法返回类型，新增影厅列表查询结果结构体

--- internal/domain/cinema/cinema_hall_cache.go
+++ internal/domain/cinema/cinema_hall_cache.go
@@ -12,7 +12,7 @@ type CinemaHallCache interface {
 	GetCinemaHall(ctx context.Context, id vo.CinemaHallID) (*CinemaHall, error)
 	SetCinemaHall(ctx context.Context, cinemaHall *CinemaHall, expiration time.Duration) error
 	DeleteCinemaHall(ctx context.Context, id vo.CinemaHallID) error
-	GetAllCinemaHalls(ctx context.Context) ([]*CinemaHall, error)
+	GetAllCinemaHalls(ctx context.Context) (*CinemaHallCacheListResult, error)
 	SetAllCinemaHalls(ctx context.Context, cinemaHalls []*CinemaHall, expiration time.Duration) error
 	DeleteAllCinemaHallIDs(ctx context.Context) error
 }
@@ -33,4 +33,11 @@ func GetCinemaHallCacheKey(id vo.CinemaHallID) string {
 
 func GetCinemaHallAllIDsKey() string {
 	return CinemaHallAllIDsKey
+}
+
+// CinemaHallCacheListResult 影厅列表的查询结果
+type CinemaHallCacheListResult struct {
+	CinemaHalls          []*CinemaHall     // 成功获取的影厅记录
+	AllCinemaHallIDs     []vo.CinemaHallID // 列表中所有的影厅ID
+	MissingCinemaHallIDs []vo.CinemaHallID // 缓存中未找到的影厅ID
 }
